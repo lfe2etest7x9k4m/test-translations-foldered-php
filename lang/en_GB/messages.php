8174a11f18c65d53b69b4e236cdfb99f34aa73f4
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-28 12:32:50 +0000

    Test EDGE-01: Delete key in en_GB, modify in en_US

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,5 +3,4 @@
 return [
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'welcome' => 'Welcome to our application',    'e2e_update_precedence_key' => 'en_GB original',
-];
+  'welcome' => 'Welcome to our application',];
