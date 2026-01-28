eb0ff3680ad94fea845f1cfdbcee5076c97f5fea
Localeflow <noreply@localeflow.io>
2026-01-28 11:13:54 +0000

    Automatic translations

---
+++ lang/fr/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'e2e_deletion_test_key' => 'VALEUR MODIFIÉE',
+  'goodbye' => 'Au revoir',
+  'greeting' => 'Bonjour, :name !',
+  'welcome' => 'Bienvenue sur notre application',
+];
