1546041d27c7cfbc218cc096dee16793d807f9de
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-28 10:52:25 +0000

    Test EDGE-01: Delete key in en_GB, modify in en_US

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,6 +3,4 @@
 return [
     'welcome' => 'Welcome to our application',
     'greeting' => 'Hello, :name!',
-    'goodbye' => 'Goodbye',
-    'e2e_deletion_test_key' => 'en_GB value',
-];
+    'goodbye' => 'Goodbye',];
