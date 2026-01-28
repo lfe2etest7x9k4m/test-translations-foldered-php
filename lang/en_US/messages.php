46f38201e61715e2f0016bb2bf5189ccc2ed4dc1
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-28 12:28:17 +0000

    Setup: Add/update e2e_update_precedence_key in all languages

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
+  'e2e_update_precedence_key' => 'en_US original',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
