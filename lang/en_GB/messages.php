bb06c925345b8e0a5b26cf5ac7c71f9fd7149011
Localeflow <noreply@localeflow.io>
2026-01-28 11:01:58 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'welcome' => 'Welcome to our application',
-    'greeting' => 'Hello, :name!',
-    'goodbye' => 'Goodbye',];
+  'goodbye' => 'Goodbye',
+  'greeting' => 'Hello, :name!',
+  'welcome' => 'Welcome to our application',
+];
