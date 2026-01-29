f49c5e18a03ddc80ab9dd0adcac0a1229e101a7e
Localeflow <noreply@localeflow.io>
2026-01-29 17:21:55 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769706862346' => 'Hello from basic translation test',
+  'e2e_basic_tr_1769707094209' => 'Hello from basic translation test',
   'e2e_update_precedence_key' => 'UPDATED VALUE',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769706862346' => 'Halló frá grunnþýðingarprófi',
+  'e2e_basic_tr_1769707094209' => 'Halló frá grunnþýðingarprófi',
   'e2e_update_precedence_key' => 'uppfærð gildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
