f430205201770e648bf13456553bb926db009ac3
Localeflow <noreply@localeflow.io>
2026-01-29 19:15:09 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769714030957' => 'Bonjour du test de traduction basique',
-  'e2e_delete_tr_1769714079803' => 'Valeur à supprimer',
   'e2e_modify_tr_1769714049514' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769714030957' => 'Hallo van de basistest voor vertalingen',
-  'e2e_delete_tr_1769714079803' => 'Waarde om te verwijderen',
   'e2e_modify_tr_1769714049514' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
