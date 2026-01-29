7d557cf9c75071b388bc2560d5dfa5f6e8204203
Localeflow <noreply@localeflow.io>
2026-01-29 19:34:59 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769715274086' => 'Halló frá einfaldri þýðingarprófun',
+  'e2e_modify_tr_1769715292807' => 'Upphafsgildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
