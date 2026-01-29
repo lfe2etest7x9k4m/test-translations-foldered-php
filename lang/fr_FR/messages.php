cbd4af54518653b02825dd92399a61e38cbc9986
Localeflow <noreply@localeflow.io>
2026-01-29 19:00:02 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769713100156' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769713118759' => 'Valeur modifiée pour le test de modification',
+  'e2e_pr_tr_1769713195023' => 'Valeur de test PR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769713100156' => 'Hallo van basistest voor vertalingen',
   'e2e_modify_tr_1769713118759' => 'Gewijzigde waarde voor aanpassingstest',
+  'e2e_pr_tr_1769713195023' => 'PR testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
