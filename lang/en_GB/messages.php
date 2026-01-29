1f9b0ad4ac9ca45c9b4a1bc02e4bb4590113b6bf
Localeflow <noreply@localeflow.io>
2026-01-29 21:02:58 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -2,8 +2,8 @@
 
 return [
   'e2e_basic_tr_1769720463880' => 'Hello from basic translation test',
+  'e2e_modify_tr_1769720477447' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
-    'e2e_pr_tr_1769720574433' => 'PR test value',
 ];
