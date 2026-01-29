ba710613103224353a2ef5a6d7c8ffdcff193a2f
Localeflow <noreply@localeflow.io>
2026-01-29 21:20:06 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,8 +4,8 @@ return [
   'e2e_basic_tr_1769721453618' => 'Hallo van de basistranslatietest',
   'e2e_modify_tr_1769721474225' => 'Gewijzigde waarde voor wijzigingstest',
   'e2e_pr_tr_1769721562601' => 'PR testwaarde',
+  'e2e_update_precedence_key' => 'Bijgewerkte waarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_update_precedence_key' => 'nl_NL original',
 ];
