7f38449c4e738f830d550ff72dec4e3176ceae92
Localeflow <noreply@localeflow.io>
2026-01-29 18:31:58 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -2,9 +2,9 @@
 
 return [
   'e2e_basic_tr_1769711459142' => 'Hello from basic translation test',
+  'e2e_delete_tr_1769711511605' => 'Value to delete',
   'e2e_modify_tr_1769711474688' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
-    'e2e_delete_tr_1769711511605' => 'Value to delete',
 ];
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769711459142' => 'Bonjour du test de traduction basique',
+  'e2e_delete_tr_1769711511605' => 'Valeur à supprimer',
   'e2e_modify_tr_1769711474688' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
