e58969893bab72abef8d6ca8c26267ab956050d3
Localeflow <noreply@localeflow.io>
2026-01-29 20:27:37 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
