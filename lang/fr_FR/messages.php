6941d8a7152d7521cc0fd695b337f15142eb16ae
Localeflow <noreply@localeflow.io>
2026-01-29 19:24:42 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769714657222' => 'Bonjour du test de traduction de base',
+  'e2e_modify_tr_1769714675905' => 'Valeur initiale pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769714657222' => 'Halló frá grunnþýðingarprófi',
+  'e2e_modify_tr_1769714675905' => 'Upphafsgildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
