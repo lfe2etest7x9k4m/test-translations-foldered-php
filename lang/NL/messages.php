0a4178be89f5d65f699fdc83c5da6a3bbdd0ed7c
Localeflow <noreply@localeflow.io>
2026-01-29 17:44:58 +0000

    Automatic translations

--- lang/NL/messages.php
+++ lang/NL/messages.php
@@ -5,6 +5,7 @@ return [
   'e2e_basic_tr_1769707094209' => 'Hallo van de basisvertalingstest',
   'e2e_basic_tr_1769707320587' => 'Hallo van de basisvertalingstest',
   'e2e_basic_tr_1769707777303' => 'Hallo van de basisvertalingstest',
+  'e2e_basic_tr_1769708686842' => 'Hallo van de basisvertalingstest',
   'e2e_update_precedence_key' => 'nl_NL original',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
