93ce0fcdc32df5bdc9ad1b5f369f05db54d1448b
Localeflow <noreply@localeflow.io>
2026-01-29 17:57:22 +0000

    Automatic translations

---
+++ lang/fr/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'e2e_basic_tr_1769706862346' => 'Bonjour du test de traduction de base',
+  'e2e_basic_tr_1769707094209' => 'Bonjour du test de traduction de base',
+  'e2e_basic_tr_1769707320587' => 'Bonjour du test de traduction de base',
+  'e2e_basic_tr_1769707777303' => 'Bonjour du test de traduction de base',
+  'e2e_basic_tr_1769708686842' => 'Bonjour du test de traduction de base',
+  'e2e_update_precedence_key' => 'en_US original',
+  'goodbye' => 'Au revoir',
+  'greeting' => 'Bonjour, :name !',
+  'welcome' => 'Bienvenue sur notre application',
+];
