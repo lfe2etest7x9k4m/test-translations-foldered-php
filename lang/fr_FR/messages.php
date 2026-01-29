631ea81e8577579b8e2fcf3419688c4be30a6579
Localeflow <noreply@localeflow.io>
2026-01-29 21:41:19 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769722860101' => 'Bonjour du test de traduction basique',
+  'e2e_modify_tr_1769722873695' => 'Valeur initiale pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769722860101' => 'Hallo van de basale vertaaltest',
+  'e2e_modify_tr_1769722873695' => 'Initiële waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
