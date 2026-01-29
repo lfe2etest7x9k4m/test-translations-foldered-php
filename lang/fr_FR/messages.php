cc3a073eed5c2c043b6122721d743a6d5e05bbde
Localeflow <noreply@localeflow.io>
2026-01-29 21:43:28 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1769722860101' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769722873695' => 'Valeur modifiée pour le test de modification',
   'e2e_pr_tr_1769722971490' => 'Valeur de test PR',
+  'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,8 +4,8 @@ return [
   'e2e_basic_tr_1769722860101' => 'Hallo van de basale vertaaltest',
   'e2e_modify_tr_1769722873695' => 'Gewijzigde waarde voor wijzigingstest',
   'e2e_pr_tr_1769722971490' => 'PR testwaarde',
+  'e2e_update_precedence_key' => 'nl_NL original',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_update_precedence_key' => 'nl_NL original',
 ];
