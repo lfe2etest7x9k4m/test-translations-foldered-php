e906c11c62735113b594d15f75c79ae7042f5284
Localeflow <noreply@localeflow.io>
2026-01-29 18:33:11 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,8 +4,8 @@ return [
   'e2e_basic_tr_1769711459142' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769711474688' => 'Breytt gildi fyrir breytingapróf',
   'e2e_pr_tr_1769711546725' => 'PR prófgildi',
+  'e2e_update_precedence_key' => 'uppfærð gildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,8 +4,8 @@ return [
   'e2e_basic_tr_1769711459142' => 'Hallo van de basistest voor vertalingen',
   'e2e_modify_tr_1769711474688' => 'Gewijzigde waarde voor wijzigingstest',
   'e2e_pr_tr_1769711546725' => 'PR testwaarde',
+  'e2e_update_precedence_key' => 'UPDATED VALUE',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_update_precedence_key' => 'nl_NL original',
 ];
