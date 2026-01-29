61012afe493c042d4bb78a8f8871380c8e85eec5
Localeflow <noreply@localeflow.io>
2026-01-29 20:00:55 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769716790238' => 'Bonjour du test de traduction de base',
+  'e2e_delete_tr_1769716841020' => 'Valeur à supprimer',
   'e2e_modify_tr_1769716803900' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769716790238' => 'Hallo vanuit de basisvertalingstest',
+  'e2e_delete_tr_1769716841020' => 'Te verwijderen waarde',
   'e2e_modify_tr_1769716803900' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
