a299fbae8f36ca5af20731ad16ad082275f86216
Localeflow <noreply@localeflow.io>
2026-01-29 19:34:40 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1769715274086' => 'Halló frá einfaldri þýðingarprófun',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
