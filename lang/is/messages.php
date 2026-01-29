46faf9a792b66ef4050c9442af250cbde10dfd2b
Localeflow <noreply@localeflow.io>
2026-01-29 21:18:01 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769721453618' => 'Halló frá grunnþýðingarprófi',
+  'e2e_modify_tr_1769721474225' => 'Upphafsgildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769721453618' => 'Hallo van de basistranslatietest',
+  'e2e_modify_tr_1769721474225' => 'Initiële waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
