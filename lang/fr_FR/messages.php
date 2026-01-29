5ccf90a5cf06c08933080b956ffa8d6e83a230e3
Localeflow <noreply@localeflow.io>
2026-01-29 21:50:25 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1769723263092' => 'Bonjour, test de traduction basique',
   'e2e_modify_tr_1769723276611' => 'Valeur modifiée pour le test de modification',
   'e2e_pr_tr_1769723374550' => 'Valeur de test PR',
+  'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
