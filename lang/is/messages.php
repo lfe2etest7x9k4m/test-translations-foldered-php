737850e66d26997acbc154b84f024208e0e6020b
Localeflow <noreply@localeflow.io>
2026-01-29 18:24:28 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769711003490' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1769711042022' => 'Gildi til að eyða',
   'e2e_modify_tr_1769711019217' => 'Upphafsgildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769711003490' => 'Hallo van de basisvertalingstest',
-  'e2e_delete_tr_1769711042022' => 'Te verwijderen waarde',
   'e2e_modify_tr_1769711019217' => 'Initiële waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
