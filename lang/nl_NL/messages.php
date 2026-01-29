d6a36a61c04c38a4eb604076af5634da3a1e01fa
Localeflow <noreply@localeflow.io>
2026-01-29 19:50:45 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769716117057' => 'Hallo van de basis vertaaltest',
   'e2e_modify_tr_1769716137720' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_pr_tr_1769716239050' => 'PR testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
