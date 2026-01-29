d7f3ee38025779cbf4f6d66a2b32e94cc160705d
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-29 19:44:41 +0000

    Reset test repo to clean state

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769715738246' => 'Hallo van de basisvertalingstest',
-  'e2e_modify_tr_1769715757077' => 'Gewijzigde waarde voor wijzigingstest',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
