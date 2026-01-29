ad46253d2371d95202bcdd5c640e78683499ebdc
Localeflow <noreply@localeflow.io>
2026-01-29 18:19:29 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769710698508' => 'Hello from basic translation test',  'e2e_modify_tr_1769710721192' => 'Modified value for modify test',
+  'e2e_basic_tr_1769710698508' => 'Hello from basic translation test',
+  'e2e_modify_tr_1769710721192' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769710698508' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1769710751129' => 'Gildi til að eyða',
   'e2e_modify_tr_1769710721192' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
