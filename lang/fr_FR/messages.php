ceed84ec50951b6e90195f1592242aca95ee0558
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-29 19:21:42 +0000

    Reset test repo to clean state

--- lang/fr_FR/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-  'e2e_basic_tr_1769714376512' => 'Bonjour du test de traduction basique',
-  'e2e_modify_tr_1769714402407' => 'Valeur modifiée pour le test de modification',
-  'goodbye' => 'Au revoir',
-  'greeting' => 'Bonjour, :name !',
-  'welcome' => 'Bienvenue sur notre application',
-];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769714376512' => 'Hallo van de basale vertaaltest',
-  'e2e_modify_tr_1769714402407' => 'Gewijzigde waarde voor wijzigingstest',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
