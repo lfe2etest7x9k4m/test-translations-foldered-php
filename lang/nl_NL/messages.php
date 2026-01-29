6ca5a2122b854b93cbd8b9a24bfbc1b7443dfbf7
Localeflow <noreply@localeflow.io>
2026-01-29 20:58:08 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769720197665' => 'Hallo van de basale vertaaltest',
-  'e2e_delete_tr_1769720233909' => 'Te verwijderen waarde',
   'e2e_modify_tr_1769720211195' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
