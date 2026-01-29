7f122c48cefb1b2f8edc72bf6e79dd348d3ad85a
Localeflow <noreply@localeflow.io>
2026-01-29 20:13:14 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769717560764' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1769717574381' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_modify_tr_1769717574381' => 'Upphafsgildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
