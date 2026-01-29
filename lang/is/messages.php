926910007c9613fcdda466e60dc5919fbf874ccf
Localeflow <noreply@localeflow.io>
2026-01-29 18:32:32 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769711459142' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769711474688' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_pr_tr_1769711546725' => 'PR prófgildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
