424d63b43f29e2096fd32e0ef1557030ba7fc85a
Localeflow <noreply@localeflow.io>
2026-01-29 21:07:56 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,7 +2,8 @@
 
 return [
   'e2e_basic_tr_1769720772633' => 'Hello from basic translation test',
-  'e2e_modify_tr_1769720786376' => 'Initial value for modify test',
+  'e2e_delete_tr_1769720809431' => 'Value to delete',
+  'e2e_modify_tr_1769720786376' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
