9c6643b733c1df8a1dc75b6702e9eb1d9a87c1dc
Localeflow <noreply@localeflow.io>
2026-01-29 20:17:56 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
+  'e2e_basic_tr_1769717868479' => 'Hello from basic translation test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
-    'e2e_basic_tr_1769717868479' => 'Hello from basic translation test',
 ];
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1769717868479' => 'Hello from basic translation test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
