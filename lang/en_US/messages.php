5953f41f1ca3202d70eaa36052f0341ba8ce8b85
Localeflow <noreply@localeflow.io>
2026-01-30 01:55:20 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769738068454' => 'Hello from basic translation test',
+  'e2e_delete_tr_1769738104935' => 'Value to delete',
   'e2e_modify_tr_1769738082097' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769738068454' => 'Hallo van de basis vertalingstest',
+  'e2e_delete_tr_1769738104935' => 'Te verwijderen waarde',
   'e2e_modify_tr_1769738082097' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
