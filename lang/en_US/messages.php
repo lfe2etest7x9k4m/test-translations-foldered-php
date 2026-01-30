ce967572ab2e36a4f4a0245a5240c16d84ba8745
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-30 04:48:18 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769748060012' => 'Hello from basic translation test',
-  'e2e_modify_tr_1769748073600' => 'Initial value for modify test',
-  'e2e_pr_tr_1769748213446' => 'PR test value',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769748060012' => 'Hallo van de basistest voor vertalingen',
-  'e2e_modify_tr_1769748073600' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_pr_tr_1769748213446' => 'PR-testwaarde',
-  'e2e_update_precedence_key' => 'Bijgewerkte waarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
