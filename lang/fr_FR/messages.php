23f3bc23a96e3cf6d7e8f0a51a360fab7f852b8b
Localeflow <noreply@localeflow.io>
2026-01-30 04:41:27 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769748060012' => 'Bonjour du test de traduction basique',
-  'e2e_modify_tr_1769748073600' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1769748073600' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769748060012' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_modify_tr_1769748073600' => 'Upphafsgildi fyrir breytingapróf',
+  'e2e_modify_tr_1769748073600' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
