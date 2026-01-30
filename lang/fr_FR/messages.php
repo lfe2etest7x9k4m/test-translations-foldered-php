e5422faddc9cb9a4f0dda066a9edb160fe140a88
Localeflow <noreply@localeflow.io>
2026-01-30 04:01:29 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769745638647' => 'Bonjour du test de traduction de base',
+  'e2e_delete_tr_1769745675169' => 'Valeur à supprimer',
   'e2e_modify_tr_1769745652301' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769745638647' => 'Halló frá grunnþýðingarprófi',
+  'e2e_delete_tr_1769745675169' => 'Gildi til að eyða',
   'e2e_modify_tr_1769745652301' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
