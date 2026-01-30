58548ccb7133d8383024b2464c338171010d7483
Localeflow <noreply@localeflow.io>
2026-01-30 04:06:16 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769745948853' => 'Bonjour du test de traduction basique',
-  'e2e_modify_tr_1769745962447' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1769745962447' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769745948853' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1769745962447' => 'Upphafsgildi fyrir breytingapróf',
+  'e2e_modify_tr_1769745962447' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
