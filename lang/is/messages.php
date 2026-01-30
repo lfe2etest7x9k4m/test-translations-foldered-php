1e22bb517d06e992890ee85069fc50f1aa29c38f
Localeflow <noreply@localeflow.io>
2026-01-30 04:52:02 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769748702349' => 'Halló frá grunnþýðingarprófi',
+  'e2e_modify_tr_1769748715830' => 'Upphafsgildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769748702349' => 'Hallo van de basale vertaaltest',
+  'e2e_modify_tr_1769748715830' => 'Initiële waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
