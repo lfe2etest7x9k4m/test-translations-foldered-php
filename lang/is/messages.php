0a237eb25f623ee0780b5542673dd4e5f8330a32
Localeflow <noreply@localeflow.io>
2026-01-30 04:34:09 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,8 +4,8 @@ return [
   'e2e_basic_tr_1769747446816' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769747467507' => 'Breytt gildi fyrir breytingapróf',
   'e2e_pr_tr_1769747613283' => 'PR prófunargildi',
+  'e2e_update_precedence_key' => 'UPDATED VALUE',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
