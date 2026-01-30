1e337b42d9a01cf6358a6a97dde25149848d1acd
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-30 03:24:01 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769741266558' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1769741280237' => 'Breytt gildi fyrir breytingapróf',
-  'e2e_pr_tr_1769741406728' => 'PR prófgildi',
-  'e2e_update_precedence_key' => 'Uppfærð gildi',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769741266558' => 'Hallo van basale vertalingstest',
-  'e2e_modify_tr_1769741280237' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_pr_tr_1769741406728' => 'PR testwaarde',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
