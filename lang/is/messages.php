e5887eb816e44a50ba9b01696e6e91821752fb2a
Localeflow <noreply@localeflow.io>
2026-01-30 04:33:39 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769747446816' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769747467507' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_pr_tr_1769747613283' => 'PR prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769747446816' => 'Hallo van de basisvertalingstest',
   'e2e_modify_tr_1769747467507' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_pr_tr_1769747613283' => 'PR testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
