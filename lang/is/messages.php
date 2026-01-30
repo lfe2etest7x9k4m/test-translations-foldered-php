eae3d57f7015c5895f8259c21604ae55f4561554
Localeflow <noreply@localeflow.io>
2026-01-30 02:50:13 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769741266558' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769741280237' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_pr_tr_1769741406728' => 'PR prófgildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
