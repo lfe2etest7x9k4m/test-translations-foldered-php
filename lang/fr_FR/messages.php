2443ebbe9c449b440c495fdf6cb4ba7db9928042
Localeflow <noreply@localeflow.io>
2026-01-30 04:52:32 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769748702349' => 'Bonjour du test de traduction basique',
+  'e2e_delete_tr_1769748738700' => 'Valeur à supprimer',
   'e2e_modify_tr_1769748715830' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769748702349' => 'Hallo van de basale vertaaltest',
+  'e2e_delete_tr_1769748738700' => 'Waarde om te verwijderen',
   'e2e_modify_tr_1769748715830' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
