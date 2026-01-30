c3601b7c3e6fcc61687f424c2a93c0aee8509bc0
Localeflow <noreply@localeflow.io>
2026-01-30 02:15:51 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769739324785' => 'Halló frá grunnþýðingarprófi',
+  'e2e_modify_tr_1769739345589' => 'Upphafsgildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769739324785' => 'Hallo van de basisvertalingstest',
+  'e2e_modify_tr_1769739345589' => 'Initiële waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
