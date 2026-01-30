57fed3cd2dd8c78582068d30b818f45392f39388
Localeflow <noreply@localeflow.io>
2026-01-30 03:48:03 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1769744696877' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769744710448' => 'Valeur modifiée pour le test de modification',
   'e2e_pr_tr_1769744840321' => 'Valeur de test PR',
+  'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,8 +4,8 @@ return [
   'e2e_basic_tr_1769744696877' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769744710448' => 'Breytt gildi fyrir breytingapróf',
   'e2e_pr_tr_1769744840321' => 'PR prófgildi',
+  'e2e_update_precedence_key' => 'uppfærð gildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
