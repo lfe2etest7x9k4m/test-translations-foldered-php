dca6ee034a57cfdcf468217199de81f4ab2bf774
Localeflow <noreply@localeflow.io>
2026-01-30 02:08:19 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769738746732' => 'Bonjour du test de traduction de base',
   'e2e_modify_tr_1769738760207' => 'Valeur modifiée pour le test de modification',
+  'e2e_pr_tr_1769738893261' => 'Valeur de test PR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769738746732' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769738760207' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_pr_tr_1769738893261' => 'PR prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
