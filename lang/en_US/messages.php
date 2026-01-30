79b5a52a516ba309500b603061dd63c1bbc84579
Localeflow <noreply@localeflow.io>
2026-01-30 04:51:49 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1769748702349' => 'Hello from basic translation test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
