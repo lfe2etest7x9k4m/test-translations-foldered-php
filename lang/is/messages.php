acb231a0279ccfe6419998ca105e83a468444856
Localeflow <noreply@localeflow.io>
2026-01-31 22:05:37 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769897117159' => 'Halló frá grunnþýðingarprófi',
+  'e2e_modify_tr_1769897130722' => 'Upphafsgildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
