c82526470a528d031489229cfabc9c7c0610470a
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-31 23:08:50 +0000

    E2E test: Modify e2e_modify_tr_1769900920526 in en_GB

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769900907082' => 'Hello from basic translation test',
-  'e2e_modify_tr_1769900920526' => 'Initial value for modify test',
+  'e2e_modify_tr_1769900920526' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
