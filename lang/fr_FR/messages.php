deda66fd69b2bf2700dbb418d8853b21cd8a82e0
Localeflow <noreply@localeflow.io>
2026-01-31 23:08:55 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769900907082' => 'Bonjour du test de traduction de base',
-  'e2e_modify_tr_1769900920526' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1769900920526' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769900907082' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1769900920526' => 'Upphaflegt gildi fyrir breytingarpróf',
+  'e2e_modify_tr_1769900920526' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
