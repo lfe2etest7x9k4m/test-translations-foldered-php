69e70a7dbd3654379121e52de0466daf238fe343
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-31 23:07:26 +0000

    Reset test repo to clean state

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769900479794' => 'Hello from basic translation test',  'e2e_modify_tr_1769900493260' => 'Modified value for modify test',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
