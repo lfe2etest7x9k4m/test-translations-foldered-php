a1d821e434dfafa0c616256e39b1b139006835c4
Localeflow <noreply@localeflow.io>
2026-01-31 18:38:32 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1769884497050' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769884517660' => 'Valeur modifiée pour le test de modification',
   'e2e_pr_tr_1769884676295' => 'Valeur de test PR',
+  'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'quota_test_key' => 'Valeur de test',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,9 +4,9 @@ return [
   'e2e_basic_tr_1769884497050' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769884517660' => 'Breytt gildi fyrir breytingapróf',
   'e2e_pr_tr_1769884676295' => 'PR prófgildi',
+  'e2e_update_precedence_key' => 'uppfærð gildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófgildi',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
