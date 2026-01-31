ac8fa253e3468bcbcb66cc4f76e1b292266ec40c
Localeflow <noreply@localeflow.io>
2026-01-31 18:52:16 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -5,5 +5,6 @@ return [
   'e2e_modify_tr_1769885389436' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'quota_test_key' => 'Prófgildi',
   'welcome' => 'Velkomin í forritið okkar',
 ];
