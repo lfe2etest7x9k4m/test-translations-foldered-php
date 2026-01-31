3087f15e9aa6a181edef9c480a1dca8d1edc45e0
Localeflow <noreply@localeflow.io>
2026-01-31 18:26:00 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1769883734881' => 'Bonjour du test de traduction de base',
   'e2e_modify_tr_1769883748383' => 'Valeur modifiée pour le test de modification',
   'e2e_pr_tr_1769883907736' => 'Valeur de test PR',
+  'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'quota_test_key' => 'Valeur de test',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,9 +4,9 @@ return [
   'e2e_basic_tr_1769883734881' => 'Halló frá einföldu þýðingarprófi',
   'e2e_modify_tr_1769883748383' => 'Breytt gildi fyrir breytingarpróf',
   'e2e_pr_tr_1769883907736' => 'PR prófgildi',
+  'e2e_update_precedence_key' => 'UPPFÆRT GÖGN',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófgildi',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
