6059779dc7e2839d9fd7cd20046bfeab3ba2c175
Localeflow <noreply@localeflow.io>
2026-01-31 18:49:56 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769885375754' => 'Bonjour du test de traduction basique',
+  'e2e_modify_tr_1769885389436' => 'Valeur initiale pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769885375754' => 'Halló frá grunnþýðingarprófi',
+  'e2e_modify_tr_1769885389436' => 'Upphafsgildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
