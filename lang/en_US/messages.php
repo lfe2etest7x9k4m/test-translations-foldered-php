10d5fca7dae52fa1936aae505e96ce0ee6162259
Localeflow <noreply@localeflow.io>
2026-01-31 18:52:39 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769885375754' => 'Hello from basic translation test',
   'e2e_modify_tr_1769885389436' => 'Initial value for modify test',
+  'e2e_pr_tr_1769885553206' => 'PR test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769885375754' => 'Hallo van basale vertalingstest',
   'e2e_modify_tr_1769885389436' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_pr_tr_1769885553206' => 'PR testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
