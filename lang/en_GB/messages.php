c788725f3e4032cfea485988df3284007c9b5f0d
lfe2etest7x9k4m <test@colinsouth.com>
2026-01-31 23:34:13 +0000

    Reset test repo to clean state

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,15 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769902032280' => 'Hello from basic translation test',
-  'e2e_modify_tr_1769902045852' => 'Modified value for modify test',
-  'e2e_multi_lang_1769902273683' => 'British English manual edit',
-  'e2e_pr_tr_1769902223631' => 'PR test value',
-  'e2e_quota_pause_1769902205819' => 'Quota pause test',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
-    'e2e_parse_test_1769902307542' => 'Parse error test value',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,14 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769902032280' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1769902045852' => 'Breytt gildi fyrir breytingarpróf',
-  'e2e_multi_lang_1769902273683' => 'is original',
-  'e2e_pr_tr_1769902223631' => 'PR prófunargildi',
-  'e2e_quota_pause_1769902205819' => 'Prófun á bið á kvóta',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
