f76ed0e901691d5685af9698ecf6c4f819a4e249
Localeflow <noreply@localeflow.io>
2026-01-31 21:42:42 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1769895756177' => 'Bonjour du test de traduction de base',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
