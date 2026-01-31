6d13ab4552045d198c2a51896dd5229dbd234fba
Localeflow <noreply@localeflow.io>
2026-01-31 18:35:32 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769884497050' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1769884517660' => 'Upphafsgildi fyrir breytingapróf',
+  'e2e_modify_tr_1769884517660' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769884497050' => 'Hallo van de basale vertaaltest',
-  'e2e_modify_tr_1769884517660' => 'Initiële waarde voor wijzigingstest',
+  'e2e_modify_tr_1769884517660' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
