8e940dd2090c8ec73e45deb498823a78a1f8f984
Localeflow <noreply@localeflow.io>
2026-01-31 21:45:40 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769895756177' => 'Hallo van de basistranslatietest',
   'e2e_modify_tr_1769895769788' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_pr_tr_1769895934069' => 'PR-testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
