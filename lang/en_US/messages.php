1272a8b7fe9f1f942faf4f6c9fa916ffc54b38fe
Localeflow <noreply@localeflow.io>
2026-01-31 23:17:00 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769901336115' => 'Hello from basic translation test',
-  'e2e_delete_tr_1769901379301' => 'Value to delete',
   'e2e_modify_tr_1769901349599' => 'Initial value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769901336115' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1769901379301' => 'Gildi til að eyða',
   'e2e_modify_tr_1769901349599' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
