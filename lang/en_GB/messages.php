2802faa28cdba6dc70fd19fff0f505cb3c06d96a
Localeflow <noreply@localeflow.io>
2026-01-31 23:30:59 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -5,7 +5,9 @@ return [
   'e2e_modify_tr_1769902045852' => 'Modified value for modify test',
   'e2e_pr_tr_1769902223631' => 'PR test value',
   'e2e_quota_pause_1769902205819' => 'Quota pause test',
+  'e2e_update_precedence_key' => 'UPDATED VALUE',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
-  'welcome' => 'Welcome to our application',];
+  'welcome' => 'Welcome to our application',
+];
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -5,6 +5,7 @@ return [
   'e2e_modify_tr_1769902045852' => 'Valeur modifiée pour le test de modification',
   'e2e_pr_tr_1769902223631' => 'Valeur de test PR',
   'e2e_quota_pause_1769902205819' => 'Test de pause de quota',
+  'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'quota_test_key' => 'Valeur de test',
