5f8dc7552441f2a7d297ff244184a1ca4fd4696f
Localeflow <noreply@localeflow.io>
2026-02-01 05:18:13 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769922882715' => 'Hallo van basis vertaaltest',
   'e2e_modify_tr_1769922903674' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_pr_tr_1769923086861' => 'PR-testwaarde',
   'e2e_quota_pause_1769923064189' => 'Quota pauzetest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
