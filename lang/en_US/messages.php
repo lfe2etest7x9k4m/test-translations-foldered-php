2d0d50a7364d62204170e035d0bfc2fd5bc8a2f8
Localeflow <noreply@localeflow.io>
2026-02-01 20:43:20 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769978352962' => 'Hello from basic translation test',
   'e2e_modify_tr_1769978373619' => 'Initial value for modify test',
+  'e2e_quota_pause_1769978579032' => 'Quota pause test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769978352962' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769978373619' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_quota_pause_1769978579032' => 'Prófun á bið á kvóta',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófunargildi',
