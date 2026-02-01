967b2da4aa98fe97eefb0f746d5fe5fece2ddbb6
Localeflow <noreply@localeflow.io>
2026-02-01 20:58:05 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769979184967' => 'Halló frá einföldu prófunarþýðingu',
   'e2e_modify_tr_1769979206370' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_multi_lang_1769979469011' => 'Bandarísk enska handvirk breyting',
   'e2e_pr_tr_1769979411598' => 'PR prófunargildi',
   'e2e_quota_pause_1769979390991' => 'Prófun á kvóta hléi',
   'e2e_update_precedence_key' => 'uppfærð gildi',
@@ -10,5 +11,4 @@ return [
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófunargildi',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_multi_lang_1769979469011' => 'is original',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769979184967' => 'Hallo van de basisvertalingstest',
   'e2e_modify_tr_1769979206370' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_multi_lang_1769979469011' => 'Amerikaanse Engelse handmatige bewerking',
   'e2e_pr_tr_1769979411598' => 'PR-testwaarde',
   'e2e_quota_pause_1769979390991' => 'Quota pauzetest',
   'e2e_update_precedence_key' => 'nl_NL original',
@@ -10,5 +11,4 @@ return [
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_multi_lang_1769979469011' => 'nl_NL original',
 ];
