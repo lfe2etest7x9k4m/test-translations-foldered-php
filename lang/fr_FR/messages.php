c71382eec87f85edbb6d76868430d5af1facacee
Localeflow <noreply@localeflow.io>
2026-02-01 05:28:05 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769923624372' => 'Bonjour du test de traduction basique',
-  'e2e_delete_tr_1769923660880' => 'Valeur à supprimer',
   'e2e_modify_tr_1769923637845' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769923624372' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1769923660880' => 'Gildi til að eyða',
   'e2e_modify_tr_1769923637845' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
