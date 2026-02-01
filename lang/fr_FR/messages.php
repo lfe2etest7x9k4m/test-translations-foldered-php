c3e7b10fc0f673fab9be0d71ecbab7d81b202aa4
Localeflow <noreply@localeflow.io>
2026-02-01 21:16:55 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769980405573' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769980420272' => 'Valeur modifiée pour le test de modification',
+  'e2e_pr_tr_1769980609722' => 'Valeur de test PR',
   'e2e_quota_pause_1769980589069' => 'Test de pause de quota',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769980405573' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769980420272' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_pr_tr_1769980609722' => 'PR prófunargildi',
   'e2e_quota_pause_1769980589069' => 'Próf á bið á kvóta',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
