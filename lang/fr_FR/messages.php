5c6c3d55c15c3eb0c50a518aa4f2ee341160697c
Localeflow <noreply@localeflow.io>
2026-02-01 07:00:21 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769928955025' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769928968550' => 'Valeur modifiée pour le test de modification',
+  'e2e_multi_lang_1769929205547' => 'Modification manuelle en anglais américain',
   'e2e_pr_tr_1769929148087' => 'Valeur de test PR',
   'e2e_quota_pause_1769929129083' => 'Test de pause de quota',
   'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
