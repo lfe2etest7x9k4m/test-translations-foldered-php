78eda325109afdb7daf577644d35c5ccdab6528e
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-01 21:14:24 +0000

    E2E test: Delete e2e_delete_tr_1769980442989 from en_GB

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769980405573' => 'Hello from basic translation test',
-  'e2e_delete_tr_1769980442989' => 'Value to delete',
-  'e2e_modify_tr_1769980420272' => 'Modified value for modify test',
+  'e2e_basic_tr_1769980405573' => 'Hello from basic translation test',  'e2e_modify_tr_1769980420272' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
