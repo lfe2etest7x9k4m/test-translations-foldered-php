511e8d518fdc263abebb00cdd65e2485b1997dd8
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-01 20:58:01 +0000

    Test EDGE-02: Modify same key in en_GB and en_US

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -10,5 +10,5 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_multi_lang_1769979469011' => 'en_GB original',
+    'e2e_multi_lang_1769979469011' => 'British English manual edit',
 ];
