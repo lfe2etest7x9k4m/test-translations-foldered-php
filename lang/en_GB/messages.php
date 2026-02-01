9feb0f15b9aefd0c41b7a9d620dd3bb56b2f92d0
Localeflow <noreply@localeflow.io>
2026-02-01 21:17:35 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -5,7 +5,9 @@ return [
   'e2e_modify_tr_1769980420272' => 'Modified value for modify test',
   'e2e_pr_tr_1769980609722' => 'PR test value',
   'e2e_quota_pause_1769980589069' => 'Quota pause test',
+  'e2e_update_precedence_key' => 'UPDATED VALUE',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
-  'welcome' => 'Welcome to our application',];
+  'welcome' => 'Welcome to our application',
+];
