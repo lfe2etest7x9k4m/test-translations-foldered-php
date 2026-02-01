8777f4412c8475aa82dd2bf41e6a577683f4dc63
Localeflow <noreply@localeflow.io>
2026-02-01 07:00:36 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769928955025' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769928968550' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_multi_lang_1769929205547' => 'is original',
   'e2e_pr_tr_1769929148087' => 'PR prófgildi',
   'e2e_quota_pause_1769929129083' => 'Próf á bið á kvóta',
   'e2e_update_precedence_key' => 'uppfærð gildi',
@@ -10,5 +11,4 @@ return [
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófgildi',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_multi_lang_1769929205547' => 'is original',
 ];
