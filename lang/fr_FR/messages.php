c9355841c5186be90ce4d1802f0154e9010380f5
Localeflow <noreply@localeflow.io>
2026-02-01 21:02:34 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769979709934' => 'Bonjour du test de traduction basique',
-  'e2e_modify_tr_1769979731519' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1769979731519' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1769979709934' => 'Hallo van de basis vertaaltest',
-  'e2e_modify_tr_1769979731519' => 'Initiële waarde voor wijzigingstest',
+  'e2e_modify_tr_1769979731519' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
