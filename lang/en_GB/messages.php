56da9c8ee67718f39cc2c6067de021a2bcba8257
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-01 20:57:52 +0000

    Setup EDGE-02: Add e2e_multi_lang_1769979469011

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -10,4 +10,5 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
+    'e2e_multi_lang_1769979469011' => 'en_GB original',
 ];
