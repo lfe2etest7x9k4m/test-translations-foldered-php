78a6ad990550d74314ed6f34df7fac039fd19c35
Localeflow <noreply@localeflow.io>
2026-02-01 05:51:39 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769925048757' => 'Halló frá grunnþýðingarprófi',
+  'e2e_delete_tr_1769925085105' => 'Gildi til að eyða',
   'e2e_modify_tr_1769925062280' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769925048757' => 'Hallo van basis vertaaltest',
+  'e2e_delete_tr_1769925085105' => 'Te verwijderen waarde',
   'e2e_modify_tr_1769925062280' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
