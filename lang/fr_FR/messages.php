7a1b8a791312b4b4d6a1f005f73db7061b4af980
Localeflow <noreply@localeflow.io>
2026-02-01 21:03:06 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769979709934' => 'Bonjour du test de traduction basique',
-  'e2e_delete_tr_1769979762108' => 'Valeur à supprimer',
   'e2e_modify_tr_1769979731519' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1769979709934' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1769979762108' => 'Gildi til að eyða',
   'e2e_modify_tr_1769979731519' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
