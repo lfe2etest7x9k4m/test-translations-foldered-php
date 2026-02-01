a44031ca132323ba14e6e4faf7af2aee92c1517c
Localeflow <noreply@localeflow.io>
2026-02-01 21:05:19 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769979709934' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769979731519' => 'Valeur modifiée pour le test de modification',
+  'e2e_quota_pause_1769979907651' => 'Test de pause de quota',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'quota_test_key' => 'Valeur de test',
