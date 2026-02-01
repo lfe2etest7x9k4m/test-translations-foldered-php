c60f21b2f3b9b06bb206bcce868b14a24d8545ce
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-01 20:52:13 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769978352962' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1769978373619' => 'Breytt gildi fyrir breytingapróf',
-  'e2e_quota_pause_1769978579032' => 'Prófun á bið á kvóta',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769978352962' => 'Hallo van basale vertalingstest',
-  'e2e_modify_tr_1769978373619' => 'Initiële waarde voor wijzigingstest',
-  'e2e_quota_pause_1769978579032' => 'Quota pauzetest',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
