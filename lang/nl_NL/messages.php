1271328b607f0215a578ee3880ccce272f3a8ecf
Localeflow <noreply@localeflow.io>
2026-02-01 21:13:32 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1769980405573' => 'Hallo van basale vertaaltest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
