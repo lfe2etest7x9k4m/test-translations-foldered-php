df0fb3c6eb503c4f01c850c9979417f99cfb2a5c
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-01 20:53:07 +0000

    E2E test: Add e2e_basic_tr_1769979184967 to en_GB

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,4 +4,5 @@ return [
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
+    'e2e_basic_tr_1769979184967' => 'Hello from basic translation test',
 ];
