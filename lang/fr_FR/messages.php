e91eb56084255a65b3926a1bad2ab899c0b54a62
Localeflow <noreply@localeflow.io>
2026-02-01 21:14:17 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1769980405573' => 'Bonjour du test de traduction basique',
+  'e2e_delete_tr_1769980442989' => 'Valeur à supprimer',
   'e2e_modify_tr_1769980420272' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
