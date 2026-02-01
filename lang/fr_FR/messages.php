38ad1374a8a40ea85cf4414be072c21219aaa14b
Localeflow <noreply@localeflow.io>
2026-02-01 20:56:20 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -5,5 +5,6 @@ return [
   'e2e_modify_tr_1769979206370' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
+  'quota_test_key' => 'Valeur de test',
   'welcome' => 'Bienvenue dans notre application',
 ];
