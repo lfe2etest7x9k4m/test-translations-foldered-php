77a70ddeb70092f18851edf66d554c6ca4b185f3
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-01 21:12:21 +0000

    Reset test repo to clean state

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,14 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769979709934' => 'Hello from basic translation test',
-  'e2e_modify_tr_1769979731519' => 'Modified value for modify test',
-  'e2e_multi_lang_1769979986008' => 'British English manual edit',
-  'e2e_pr_tr_1769979928642' => 'PR test value',
-  'e2e_quota_pause_1769979907651' => 'Quota pause test',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,14 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1769979709934' => 'Hallo van de basis vertaaltest',
-  'e2e_modify_tr_1769979731519' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_multi_lang_1769979986008' => 'nl_NL original',
-  'e2e_pr_tr_1769979928642' => 'PR testwaarde',
-  'e2e_quota_pause_1769979907651' => 'Quota pauze test',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
