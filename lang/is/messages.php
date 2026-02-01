160d010e22cdc47660bfbaf94ca747a6669113ab
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-01 05:31:10 +0000

    Setup EDGE-02: Add e2e_multi_lang_1769923866352

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -10,4 +10,5 @@ return [
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófunargildi',
   'welcome' => 'Velkomin í forritið okkar',
+    'e2e_multi_lang_1769923866352' => 'is original',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -10,4 +10,5 @@ return [
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
+    'e2e_multi_lang_1769923866352' => 'nl_NL original',
 ];
