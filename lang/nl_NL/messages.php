e19cca70da21d178f8131607f8f89ea5ee64693c
Localeflow <noreply@localeflow.io>
2026-02-01 21:06:12 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -5,9 +5,9 @@ return [
   'e2e_modify_tr_1769979731519' => 'Gewijzigde waarde voor wijzigingstest',
   'e2e_pr_tr_1769979928642' => 'PR testwaarde',
   'e2e_quota_pause_1769979907651' => 'Quota pauze test',
+  'e2e_update_precedence_key' => 'nl_NL original',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_update_precedence_key' => 'nl_NL original',
 ];
