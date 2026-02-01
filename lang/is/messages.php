1bff2a9ddd59973a19b9e301e38695dc12c29392
Localeflow <noreply@localeflow.io>
2026-02-01 05:54:38 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -5,9 +5,9 @@ return [
   'e2e_modify_tr_1769925062280' => 'Breytt gildi fyrir breytingapróf',
   'e2e_pr_tr_1769925241909' => 'PR prófgildi',
   'e2e_quota_pause_1769925222790' => 'Próf á stöðvun kvóta',
+  'e2e_update_precedence_key' => 'Uppfærð gildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófgildi',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
