4224a7f2b5bb234790ebcd1b8f2c511f17898b5e
Localeflow <noreply@localeflow.io>
2026-02-01 05:53:56 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769925048757' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1769925062280' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_quota_pause_1769925222790' => 'Próf á stöðvun kvóta',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófgildi',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769925048757' => 'Hallo van basis vertaaltest',
   'e2e_modify_tr_1769925062280' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_quota_pause_1769925222790' => 'Quota pauzetest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
