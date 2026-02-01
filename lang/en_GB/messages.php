816c19ff0d0932b509cb4f6266de577969440166
Localeflow <noreply@localeflow.io>
2026-02-01 21:16:41 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,9 +3,9 @@
 return [
   'e2e_basic_tr_1769980405573' => 'Hello from basic translation test',
   'e2e_modify_tr_1769980420272' => 'Modified value for modify test',
+  'e2e_quota_pause_1769980589069' => 'Quota pause test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_quota_pause_1769980589069' => 'Quota pause test',
 ];
