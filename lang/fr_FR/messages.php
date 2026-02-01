cbece4944674860943cc3ccd1969db12b034b6ba
Localeflow <noreply@localeflow.io>
2026-02-01 20:58:27 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1769979184967' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1769979206370' => 'Valeur modifiée pour le test de modification',
+  'e2e_multi_lang_1769979469011' => 'Modification manuelle en anglais britannique',
   'e2e_pr_tr_1769979411598' => 'Valeur de test PR',
   'e2e_quota_pause_1769979390991' => 'Test de pause de quota',
   'e2e_update_precedence_key' => 'VALEUR MISE À JOUR',
