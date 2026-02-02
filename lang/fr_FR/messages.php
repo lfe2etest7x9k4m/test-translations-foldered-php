5379bc4f77eb64e7fe8ee600515b29b808107d76
LocaleFlow <noreply@localeflow.io>
2026-02-02 12:47:11 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770035975725' => 'Bonjour du test de traduction de base',
   'e2e_modify_tr_1770035989482' => 'Valeur modifiée pour le test de modification',
+  'e2e_pr_tr_1770036425733' => 'Valeur de test PR',
   'e2e_rt05_ws_1770036393797' => 'Test WebSocket RT-05',
   'e2e_rt21_ws_1770036145439' => 'Test de mise à jour d\'utilisation RT-21',
   'e2e_rt24_ws_1770036120362' => 'Test de recharge automatique RT-24',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770035975725' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770035989482' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_pr_tr_1770036425733' => 'PR prófunargildi',
   'e2e_rt05_ws_1770036393797' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770036145439' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_rt24_ws_1770036120362' => 'RT-24 sjálfvirk áfyllingarpróf',
