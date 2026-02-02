df1965fad37babc29bca74dc96c113c1ba13507f
LocaleFlow <noreply@localeflow.io>
2026-02-02 13:31:34 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770039034943' => 'Hallo van basale vertalingstest',
+  'e2e_delete_tr_1770039078793' => 'Te verwijderen waarde',
   'e2e_modify_tr_1770039055808' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
