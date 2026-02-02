806da6159617857b985ec2813ca4b0b884d0183b
LocaleFlow <noreply@localeflow.io>
2026-02-02 13:43:32 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770039750915' => 'Hallo von grundlegenden Übersetzungs tests',
-  'e2e_delete_tr_1770039787694' => 'Zu löschender Wert',
   'e2e_modify_tr_1770039764548' => 'Anfangswert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770039750915' => 'Bonjour de test de traduction basique',
-  'e2e_delete_tr_1770039787694' => 'Valeur à supprimer',
   'e2e_modify_tr_1770039764548' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
