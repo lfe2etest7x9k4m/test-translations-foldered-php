622788537f5726e1e125c5d4181e4bc6d2acb54e
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-02 12:02:36 +0000

    Reset test repo to clean state

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,18 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770032892933' => 'Hello from basic translation test',
-  'e2e_modify_tr_1770032906570' => 'Modified value for modify test',
-  'e2e_multi_lang_1770033236085' => 'British English manual edit',
-  'e2e_pr_tr_1770033171214' => 'PR test value',
-  'e2e_quota_pause_1770033124578' => 'Quota pause test',
-  'e2e_rt05_ws_1770033137524' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770033063819' => 'RT-21 usage update test',
-  'e2e_rt24_ws_1770033036074' => 'RT-24 auto top-up test',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'e2e_ws_rt_1770032984563' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,18 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770032892933' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1770032906570' => 'Breytt gildi fyrir breytingapróf',
-  'e2e_multi_lang_1770033236085' => 'is original',
-  'e2e_pr_tr_1770033171214' => 'PR prófunargildi',
-  'e2e_quota_pause_1770033124578' => 'Prófun á stöðvun kvóta',
-  'e2e_rt05_ws_1770033137524' => 'RT-05 WebSocket próf',
-  'e2e_rt21_ws_1770033063819' => 'RT-21 notkunaruppfærslupróf',
-  'e2e_rt24_ws_1770033036074' => 'RT-24 sjálfvirk áfyllingarpróf',
-  'e2e_update_precedence_key' => 'UPPFÆRT GÖGN',
-  'e2e_ws_rt_1770032984563' => 'WebSocket rauntíma prófunargildi',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
