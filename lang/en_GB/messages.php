fd5ed8b9a200071c96c140a81142e6f3d18f17a6
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-02 15:35:14 +0000

    E2E test: Add e2e_err10_1770046511580 for ERR-10 push rejection fallback

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -15,4 +15,5 @@ return [
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
     'e2e_multi_lang_1770046481948' => 'British English manual edit',
+    'e2e_err10_1770046511580' => 'Push rejection test',
 ];
