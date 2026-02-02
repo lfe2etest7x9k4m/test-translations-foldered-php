24228080f1dc01d3bb1735a4518e6311496a9b82
LocaleFlow <noreply@localeflow.io>
2026-02-02 09:46:35 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770025316135' => 'Hello from basic translation test',
   'e2e_modify_tr_1770025329917' => 'Modified value for modify test',
+  'e2e_pr_tr_1770025589061' => 'PR test value',
   'e2e_quota_pause_1770025541778' => 'Quota pause test',
   'e2e_rt05_ws_1770025554734' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770025479911' => 'RT-21 usage update test',
@@ -11,5 +12,4 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_pr_tr_1770025589061' => 'PR test value',
 ];
