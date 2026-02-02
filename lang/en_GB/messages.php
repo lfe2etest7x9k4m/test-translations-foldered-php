0a0a5fb113c071f5e188362b6146271cde8495e5
LocaleFlow <noreply@localeflow.io>
2026-02-02 14:59:39 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770044101969' => 'Hello from basic translation test',
   'e2e_modify_tr_1770044115845' => 'Modified value for modify test',
+  'e2e_quota_pause_1770044333908' => 'Quota pause test',
   'e2e_rt21_ws_1770044276297' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770044247475' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770044194918' => 'WebSocket real-time test value',
@@ -10,6 +11,4 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_quota_pause_1770044333908' => 'Quota pause test',
-    'e2e_rt05_ws_1770044353955' => 'RT-05 WebSocket test',
 ];
