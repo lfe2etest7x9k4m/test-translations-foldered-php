2f19da4ae75fc2168770ee35cebaba5818264200
LocaleFlow <noreply@localeflow.io>
2026-02-02 10:02:01 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770026469388' => 'Bonjour du test de traduction basique',
+  'e2e_delete_tr_1770026506310' => 'Valeur à supprimer',
   'e2e_modify_tr_1770026483363' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
