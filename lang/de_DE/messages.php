cb768fcb67f05f76d52be9783a9f9991161ef654
LocaleFlow <noreply@localeflow.io>
2026-02-02 15:28:46 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770046105435' => 'Hallo vom grundlegenden Übersetzungstest',
+  'e2e_modify_tr_1770046119272' => 'Initieller Wert für den Änderungs-Test',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770046105435' => 'Hello from basic translation test',
+  'e2e_modify_tr_1770046119272' => 'Initial value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
