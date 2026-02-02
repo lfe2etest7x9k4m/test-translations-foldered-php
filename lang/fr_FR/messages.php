d197be8832e303847bc26c768be20e1535dab9ef
LocaleFlow <noreply@localeflow.io>
2026-02-02 13:48:52 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770039750915' => 'Bonjour de test de traduction basique',
+  'e2e_err10_1770040125731' => 'Test de rejet de push',
   'e2e_modify_tr_1770039764548' => 'Valeur modifiée pour le test de modification',
   'e2e_multi_lang_1770040082368' => 'Modification manuelle en anglais américain',
   'e2e_pr_tr_1770040030731' => 'Valeur de test PR',
