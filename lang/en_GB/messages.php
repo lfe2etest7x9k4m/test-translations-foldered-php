c46fbfaccf9debcd761dc9d07f9e797756bb79bf
LocaleFlow <noreply@localeflow.io>
2026-02-02 15:20:40 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770045320165' => 'Hello from basic translation test',
   'e2e_modify_tr_1770045340982' => 'Modified value for modify test',
   'e2e_quota_pause_1770045600376' => 'Quota pause test',
+  'e2e_rt05_ws_1770045613535' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770045536553' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770045507347' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770045453770' => 'WebSocket real-time test value',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,7 +3,7 @@
 return [
   'e2e_basic_tr_1770045320165' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770045340982' => 'Breytt gildi fyrir breytingapróf',
-  'e2e_quota_pause_1770045600376' => 'Próf á stöðvun kvóta',
+  'e2e_rt05_ws_1770045613535' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770045536553' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_rt24_ws_1770045507347' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770045453770' => 'WebSocket rauntíma prófunargildi',
