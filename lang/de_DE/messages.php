18035063e5365bd047591c83ae18b797cbb7dd48
LocaleFlow <noreply@localeflow.io>
2026-02-02 15:20:52 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770045320165' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_modify_tr_1770045340982' => 'Modifizierter Wert für Modifikationstest',
+  'e2e_pr_tr_1770045646835' => 'PR-Testwert',
   'e2e_rt05_ws_1770045613535' => 'RT-05 WebSocket-Test',
   'e2e_rt21_ws_1770045536553' => 'RT-21 Nutzungstest',
   'e2e_rt24_ws_1770045507347' => 'RT-24 automatische Nachfüllungstest',
