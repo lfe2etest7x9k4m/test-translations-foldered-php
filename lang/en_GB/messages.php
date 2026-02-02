6d2b678f451ee76690eba063fbff089461b9fad4
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-02 12:46:34 +0000

    Test RT-05: e2e_rt05_ws_1770036393797

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -11,4 +11,5 @@ return [
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
     'e2e_quota_pause_1770036380759' => 'Quota pause test',
+    'e2e_rt05_ws_1770036393797' => 'RT-05 WebSocket test',
 ];
