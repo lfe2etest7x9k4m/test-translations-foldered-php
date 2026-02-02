bba610619d2ebef6097380a857e683690865927e
LocaleFlow <noreply@localeflow.io>
2026-02-02 14:31:14 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770042644361' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1770042658048' => 'Upphaflegt gildi fyrir breytingapróf',
+  'e2e_modify_tr_1770042658048' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770042644361' => 'Hallo van basale vertaaltest',
-  'e2e_modify_tr_1770042658048' => 'Initiële waarde voor wijzigingstest',
+  'e2e_modify_tr_1770042658048' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
