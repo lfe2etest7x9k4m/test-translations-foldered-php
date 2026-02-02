efbc0911edf39252ddf62bf8ff59b0509922ad4a
LocaleFlow <noreply@localeflow.io>
2026-02-02 09:21:27 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770024059483' => 'Bonjour de test de traduction basique',
-  'e2e_modify_tr_1770024073104' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1770024073104' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770024059483' => 'Hallo van basale vertalingstest',
-  'e2e_modify_tr_1770024073104' => 'Initiële waarde voor aanpassen test',
+  'e2e_modify_tr_1770024073104' => 'Gewijzigde waarde voor aanpassen test',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
