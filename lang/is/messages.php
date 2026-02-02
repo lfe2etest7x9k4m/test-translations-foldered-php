69f3634eaa30f08692153a3ae894fa0d35abeedd
Localeflow <noreply@localeflow.io>
2026-02-02 07:04:03 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770015605645' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770015619193' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_pr_tr_1770015837361' => 'PR prófunargildi',
   'e2e_quota_pause_1770015815224' => 'Prófun á stöðvun kvóta',
   'e2e_ws_rt_1770015697088' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
