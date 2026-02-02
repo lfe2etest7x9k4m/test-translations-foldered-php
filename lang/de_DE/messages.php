f508af15b8e7baf033a9043b321e86e94a3492e3
LocaleFlow <noreply@localeflow.io>
2026-02-02 09:56:45 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -3,7 +3,7 @@
 return [
   'e2e_basic_tr_1770025940449' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_modify_tr_1770025954211' => 'Anfangswert für Modifikationstest',
-  'e2e_quota_pause_1770026165868' => 'Quota-Pausentest',
+  'e2e_rt05_ws_1770026178722' => 'RT-05 WebSocket-Test',
   'e2e_rt21_ws_1770026103857' => 'RT-21 Nutzungsaktualisierungstest',
   'e2e_ws_rt_1770026033210' => 'WebSocket-Echtzeittestwert',
   'goodbye' => 'Auf Wiedersehen',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -3,7 +3,7 @@
 return [
   'e2e_basic_tr_1770025940449' => 'Hello from basic translation test',
   'e2e_modify_tr_1770025954211' => 'Initial value for modify test',
-  'e2e_quota_pause_1770026165868' => 'Quota pause test',
+  'e2e_rt05_ws_1770026178722' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770026103857' => 'RT-21 usage update test',
   'e2e_ws_rt_1770026033210' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
