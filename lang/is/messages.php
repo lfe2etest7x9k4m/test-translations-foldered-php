babdde41f3d9daa21d5971468b9efa4dab32229e
LocaleFlow <noreply@localeflow.io>
2026-02-02 14:55:54 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770044101969' => 'Halló frá grunnþýðingarprófi',
+  'e2e_delete_tr_1770044138998' => 'Gildi til að eyða',
   'e2e_modify_tr_1770044115845' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770044101969' => 'Hallo van de basisvertalingstest',
+  'e2e_delete_tr_1770044138998' => 'Te verwijderen waarde',
   'e2e_modify_tr_1770044115845' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
