b15e275b13d4c63e008ebdeaf0322be3d275457c
LocaleFlow <noreply@localeflow.io>
2026-02-02 15:35:17 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,9 @@
 
 return [
   'e2e_basic_tr_1770046105435' => 'Hallo van basale vertaaltest',
+  'e2e_err10_1770046511580' => 'Push afwijzingstest',
   'e2e_modify_tr_1770046119272' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_multi_lang_1770046481948' => 'nl_NL original',
   'e2e_pr_tr_1770046403345' => 'PR testwaarde',
   'e2e_quota_pause_1770046340551' => 'Quota pauzetest',
   'e2e_rt05_ws_1770046353642' => 'RT-05 WebSocket test',
@@ -14,5 +16,4 @@ return [
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_multi_lang_1770046481948' => 'nl_NL original',
 ];
