5a5b1342379e5ab6ae05c9554af815466d7017dd
LocaleFlow <noreply@localeflow.io>
2026-02-02 15:03:11 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770044101969' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_modify_tr_1770044115845' => 'Anfangswert für Modifikationstest',
   'e2e_pr_tr_1770044400266' => 'PR-Testwert',
+  'e2e_quota_pause_1770044333908' => 'Kontingentpausentest',
   'e2e_rt05_ws_1770044353955' => 'RT-05 WebSocket-Test',
   'e2e_rt21_ws_1770044276297' => 'RT-21 Test zur Nutzungsaktualisierung',
   'e2e_rt24_ws_1770044247475' => 'RT-24 automatische Nachfüllungstest',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770044101969' => 'Hello from basic translation test',
   'e2e_modify_tr_1770044115845' => 'Initial value for modify test',
   'e2e_pr_tr_1770044400266' => 'PR test value',
+  'e2e_quota_pause_1770044333908' => 'Quota pause test',
   'e2e_rt05_ws_1770044353955' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770044276297' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770044247475' => 'RT-24 auto top-up test',
