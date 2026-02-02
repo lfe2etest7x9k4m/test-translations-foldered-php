1bbc6903a44b55c9130c60d2cd75213223316a3e
LocaleFlow <noreply@localeflow.io>
2026-02-02 12:42:05 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770035975725' => 'Hello from basic translation test',
   'e2e_modify_tr_1770035989482' => 'Initial value for modify test',
+  'e2e_rt24_ws_1770036120362' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770036067750' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770035975725' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770035989482' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_rt24_ws_1770036120362' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770036067750' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
