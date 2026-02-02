862b35412db1edd26914298b8f257070fd13ea88
LocaleFlow <noreply@localeflow.io>
2026-02-02 13:05:08 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770037449273' => 'Hello from basic translation test',
+  'e2e_delete_tr_1770037492967' => 'Value to delete',
   'e2e_modify_tr_1770037469978' => 'Initial value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770037449273' => 'Halló frá grunnþýðingarprófi',
+  'e2e_delete_tr_1770037492967' => 'Gildi til að eyða',
   'e2e_modify_tr_1770037469978' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
