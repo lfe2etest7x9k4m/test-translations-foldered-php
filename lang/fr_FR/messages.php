3b31a1722fbbd888ff16c618215a704fdbecd1d3
LocaleFlow <noreply@localeflow.io>
2026-02-02 14:09:37 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770041268429' => 'Bonjour du test de traduction de base',
   'e2e_modify_tr_1770041282189' => 'Valeur initiale pour le test de modification',
+  'e2e_ws_rt_1770041368049' => 'Valeur de test en temps réel WebSocket',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770041268429' => 'Hallo van basis vertaaltest',
   'e2e_modify_tr_1770041282189' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_ws_rt_1770041368049' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
