4c1074c673699e74f8d65fe4dea021a4f9acb813
LocaleFlow <noreply@localeflow.io>
2026-02-02 11:48:33 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770032892933' => 'Hallo vom grundlegenden Übersetzungstest',
+  'e2e_modify_tr_1770032906570' => 'Anfangswert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770032892933' => 'Halló frá grunnþýðingarprófi',
+  'e2e_modify_tr_1770032906570' => 'Upphaflegt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
