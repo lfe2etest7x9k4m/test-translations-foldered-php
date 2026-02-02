dee5a85f215cc738ef3d37e1492273723cc91727
LocaleFlow <noreply@localeflow.io>
2026-02-02 11:31:46 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770031757596' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770031771344' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_rt24_ws_1770031901733' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770031850145' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770031757596' => 'Hallo van basistest voor vertaling',
   'e2e_modify_tr_1770031771344' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_rt24_ws_1770031901733' => 'RT-24 automatische bijvultest',
   'e2e_ws_rt_1770031850145' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
