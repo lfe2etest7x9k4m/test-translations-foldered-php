fb98d84624dfaa459311152afec2ab6a643b20af
LocaleFlow <noreply@localeflow.io>
2026-02-02 09:57:36 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -6,10 +6,10 @@ return [
   'e2e_pr_tr_1770026211779' => 'PR prófunargildi',
   'e2e_rt05_ws_1770026178722' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770026103857' => 'RT-21 notkunaruppfærsla próf',
+  'e2e_update_precedence_key' => 'UPPFÆRT GÖGN',
   'e2e_ws_rt_1770026033210' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófunargildi',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
