facdac6f3cbfc62f8c5dea1b3df09bcc29d2811a
LocaleFlow <noreply@localeflow.io>
2026-02-02 14:31:46 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770042644361' => 'Hallo von grundlegenden Übersetzungstest',
-  'e2e_delete_tr_1770042680897' => 'Zu löschender Wert',
   'e2e_modify_tr_1770042658048' => 'Anfangswert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770042644361' => 'Hallo van basale vertaaltest',
-  'e2e_delete_tr_1770042680897' => 'Te verwijderen waarde',
   'e2e_modify_tr_1770042658048' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
