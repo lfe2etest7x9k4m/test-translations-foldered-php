1c5c5b57868e01ad42fde0d8bb67f3d5ce0fdb37
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-02 11:35:23 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770031757596' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1770031771344' => 'Breytt gildi fyrir breytingarpróf',
-  'e2e_rt24_ws_1770031901733' => 'RT-24 sjálfvirk áfyllingarpróf',
-  'e2e_ws_rt_1770031850145' => 'WebSocket rauntíma prófunargildi',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770031757596' => 'Hallo van basistest voor vertaling',
-  'e2e_modify_tr_1770031771344' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_rt24_ws_1770031901733' => 'RT-24 automatische bijvultest',
-  'e2e_ws_rt_1770031850145' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
