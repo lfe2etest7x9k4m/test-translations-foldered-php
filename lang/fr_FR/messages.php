159a8cbe98d433d8de52e941fdb70b60878e947d
LocaleFlow <noreply@localeflow.io>
2026-02-02 10:01:37 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770026469388' => 'Bonjour du test de traduction basique',
-  'e2e_modify_tr_1770026483363' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1770026483363' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue dans notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770026469388' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1770026483363' => 'Upphafsgildi fyrir breytingapróf',
+  'e2e_modify_tr_1770026483363' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
