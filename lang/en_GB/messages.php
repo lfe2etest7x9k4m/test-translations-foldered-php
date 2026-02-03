a856b2aac8823160524ec5a33e61d8fdc8485a9b
LocaleFlow <noreply@localeflow.io>
2026-02-03 18:56:11 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770144685993' => 'Hello from basic translation test',
   'e2e_modify_tr_1770144707002' => 'Modified value for modify test',
+  'e2e_quota_pause_1770144934004' => 'Quota pause test',
   'e2e_rt21_ws_1770144872778' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770144845122' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770144792466' => 'WebSocket real-time test value',
@@ -10,6 +11,4 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_quota_pause_1770144934004' => 'Quota pause test',
-    'e2e_rt05_ws_1770144946972' => 'RT-05 WebSocket test',
 ];
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770144685993' => 'Hello from basic translation test',
   'e2e_modify_tr_1770144707002' => 'Initial value for modify test',
+  'e2e_quota_pause_1770144934004' => 'Quota pause test',
   'e2e_rt21_ws_1770144872778' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770144845122' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770144792466' => 'WebSocket real-time test value',
