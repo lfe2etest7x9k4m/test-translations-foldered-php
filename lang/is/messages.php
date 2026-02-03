078769deb823d197fb87b6e115d21d60e601d28c
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-03 09:06:19 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,20 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770046105435' => 'Halló frá grunnþýðingarprófi',
-  'e2e_bp03_1770046530252' => 'GitHub verndarpróf',
-  'e2e_err10_1770046511580' => 'Prófun á höfnun á ýtingu',
-  'e2e_modify_tr_1770046119272' => 'Breytt gildi fyrir breytingapróf',
-  'e2e_multi_lang_1770046481948' => 'is original',
-  'e2e_pr_tr_1770046403345' => 'PR prófunargildi',
-  'e2e_quota_pause_1770046340551' => 'Kvóta biðpróf',
-  'e2e_rt05_ws_1770046353642' => 'RT-05 WebSocket próf',
-  'e2e_rt21_ws_1770046279181' => 'RT-21 notkunaruppfærslupróf',
-  'e2e_rt24_ws_1770046251496' => 'RT-24 sjálfvirk áfyllingarpróf',
-  'e2e_update_precedence_key' => 'UPPFÆRT GÖGN',
-  'e2e_ws_rt_1770046197907' => 'WebSocket rauntíma prófunargildi',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,20 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770046105435' => 'Hallo van basale vertaaltest',
-  'e2e_bp03_1770046530252' => 'GitHub beschermingstest',
-  'e2e_err10_1770046511580' => 'Push afwijzingstest',
-  'e2e_modify_tr_1770046119272' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_multi_lang_1770046481948' => 'nl_NL original',
-  'e2e_pr_tr_1770046403345' => 'PR testwaarde',
-  'e2e_quota_pause_1770046340551' => 'Quota pauzetest',
-  'e2e_rt05_ws_1770046353642' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770046279181' => 'RT-21 gebruiksupdate test',
-  'e2e_rt24_ws_1770046251496' => 'RT-24 automatische bijvulling test',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'e2e_ws_rt_1770046197907' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
