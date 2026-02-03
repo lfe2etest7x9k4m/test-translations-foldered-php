ae51ae1b38eea3ab5908bc913fd09538b765dac3
LocaleFlow <noreply@localeflow.io>
2026-02-03 09:20:00 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770110220096' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_modify_tr_1770110233895' => 'Anfangswert für Modifikationstest',
+  'e2e_rt21_ws_1770110395873' => 'RT-21 Nutzungstest für Updates',
   'e2e_rt24_ws_1770110367526' => 'RT-24 automatische Nachfüllungstest',
   'e2e_ws_rt_1770110313185' => 'WebSocket Echtzeit-Testwert',
   'goodbye' => 'Auf Wiedersehen',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770110220096' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770110233895' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_rt21_ws_1770110395873' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_rt24_ws_1770110367526' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770110313185' => 'WebSocket rauntíma prófgildi',
   'goodbye' => 'Bless',
