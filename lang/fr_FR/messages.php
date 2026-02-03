882428f275e76869b9b8f663003b6b5c9def9792
LocaleFlow <noreply@localeflow.io>
2026-02-03 09:21:38 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770110220096' => 'Bonjour du test de traduction de base',
   'e2e_modify_tr_1770110233895' => 'Valeur modifiée pour le test de modification',
+  'e2e_quota_pause_1770110458325' => 'Test de pause de quota',
   'e2e_rt21_ws_1770110395873' => 'Test de mise à jour d\'utilisation RT-21',
   'e2e_rt24_ws_1770110367526' => 'Test de recharge automatique RT-24',
   'e2e_ws_rt_1770110313185' => 'Valeur de test en temps réel WebSocket',
