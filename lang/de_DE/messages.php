bfc56865741384f322e0436f6a192050ed9455f7
LocaleFlow <noreply@localeflow.io>
2026-02-04 12:55:17 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770209358437' => 'Hallo von grundlegenden Übersetzungstest',
   'e2e_modify_tr_1770209372139' => 'Ursprünglicher Wert für Modifikationstest',
   'e2e_pr_tr_1770209638698' => 'PR-Testwert',
+  'e2e_quota_pause_1770209593088' => 'Kontingentpausentest',
   'e2e_rt05_ws_1770209606101' => 'RT-05 WebSocket-Test',
   'e2e_rt21_ws_1770209531111' => 'RT-21 Test für Nutzungsaktualisierung',
   'e2e_rt24_ws_1770209503470' => 'RT-24 automatische Nachfüllungstest',
