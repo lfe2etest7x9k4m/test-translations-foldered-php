905b044b543d7ba710a5afff6fe549d2ea44fede
LocaleFlow <noreply@localeflow.io>
2026-02-04 12:54:04 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770209358437' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770209372139' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_pr_tr_1770209638698' => 'PR prófunargildi',
   'e2e_rt05_ws_1770209606101' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770209531111' => 'RT-21 notkunaruppfærslupróf',
   'e2e_rt24_ws_1770209503470' => 'RT-24 sjálfvirk áfyllingarpróf',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770209358437' => 'Hallo van basistest vertaling',
   'e2e_modify_tr_1770209372139' => 'Initiële waarde voor wijzigingstest',
+  'e2e_pr_tr_1770209638698' => 'PR testwaarde',
   'e2e_rt05_ws_1770209606101' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770209531111' => 'RT-21 test voor gebruiksupdate',
   'e2e_rt24_ws_1770209503470' => 'RT-24 automatische bijvultest',
