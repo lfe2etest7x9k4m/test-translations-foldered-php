ba15544b467ad1f27098fd39e2e3513fd34f89bc
LocaleFlow <noreply@localeflow.io>
2026-02-05 12:36:42 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770294868553' => 'Bonjour du test de traduction basique',
   'e2e_modify_tr_1770294882433' => 'Valeur initiale pour le test de modification',
+  'e2e_rt24_ws_1770294997364' => 'Test de recharge automatique RT-24',
   'e2e_ws_rt_1770294960524' => 'Valeur de test en temps réel WebSocket',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770294868553' => 'Hallo van basis vertaaltest',
   'e2e_modify_tr_1770294882433' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_rt24_ws_1770294997364' => 'RT-24 automatische bijvultest',
   'e2e_ws_rt_1770294960524' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
