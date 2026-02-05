6614b9b2c48f42b95be084033379589f231af947
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-05 02:43:33 +0000

    Test QUOTA-05: e2e_quota_pause_1770259412995

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -10,4 +10,5 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
+    'e2e_quota_pause_1770259412995' => 'Quota pause test',
 ];
