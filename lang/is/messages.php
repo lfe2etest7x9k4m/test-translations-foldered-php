652c1890f4236f999915db6670c2639f2e8bc23e
LocaleFlow <noreply@localeflow.io>
2026-02-05 02:44:09 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,7 +3,7 @@
 return [
   'e2e_basic_tr_1770259106183' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770259119788' => 'Breytt gildi fyrir breytingapróf',
-  'e2e_quota_pause_1770259412995' => 'Próf á stöðvun kvóta',
+  'e2e_rt05_ws_1770259424928' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770259367381' => 'RT-21 próf fyrir notkunaruppfærslu',
   'e2e_rt24_ws_1770259233550' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770259196833' => 'WebSocket rauntíma prófunargildi',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,7 +3,7 @@
 return [
   'e2e_basic_tr_1770259106183' => 'Hallo van basale vertaaltest',
   'e2e_modify_tr_1770259119788' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_quota_pause_1770259412995' => 'Quotum pauze test',
+  'e2e_rt05_ws_1770259424928' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770259367381' => 'RT-21 test voor gebruiksupdate',
   'e2e_rt24_ws_1770259233550' => 'RT-24 automatische bijvultest',
   'e2e_ws_rt_1770259196833' => 'WebSocket real-time testwaarde',
