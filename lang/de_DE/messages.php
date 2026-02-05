1f6682d4c05439ce8281b04ff5753ed42451de31
LocaleFlow <noreply@localeflow.io>
2026-02-05 02:39:17 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770259106183' => 'Hallo vom grundlegenden Übersetzungstest',
+  'e2e_delete_tr_1770259142529' => 'Zu löschender Wert',
   'e2e_modify_tr_1770259119788' => 'Anfangswert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770259106183' => 'Halló frá grunnþýðingarprófi',
+  'e2e_delete_tr_1770259142529' => 'Gildi til að eyða',
   'e2e_modify_tr_1770259119788' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
