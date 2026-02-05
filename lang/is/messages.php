b788493f114a090fee2e5827391eb0f364b7d415
LocaleFlow <noreply@localeflow.io>
2026-02-05 12:41:35 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770294868553' => 'Halló frá grunnþýðingarprófi',
   'e2e_modify_tr_1770294882433' => 'Breytt gildi fyrir breytingapróf',
   'e2e_pr_tr_1770295229172' => 'PR prófunargildi',
+  'e2e_quota_pause_1770295176798' => 'Próf á stöðvun kvóta',
   'e2e_rt05_ws_1770295188859' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770295131185' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_rt24_ws_1770294997364' => 'RT-24 sjálfvirk áfyllingarpróf',
