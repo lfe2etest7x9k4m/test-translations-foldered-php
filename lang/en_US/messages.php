81d59a68ae01a281398c6b5009203220780e6fad
LocaleFlow <noreply@localeflow.io>
2026-02-05 02:10:45 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770257384593' => 'Hello from basic translation test',
-  'e2e_delete_tr_1770257421088' => 'Value to delete',
   'e2e_modify_tr_1770257398249' => 'Initial value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770257384593' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1770257421088' => 'Gildi til að eyða',
   'e2e_modify_tr_1770257398249' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
