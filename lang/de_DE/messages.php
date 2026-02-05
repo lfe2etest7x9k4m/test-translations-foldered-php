08e1214ff5e30482a327d7ded73b0ab872cefae7
LocaleFlow <noreply@localeflow.io>
2026-02-05 12:36:08 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770294868553' => 'Hallo von grundlegenden Übersetzungstests',
   'e2e_modify_tr_1770294882433' => 'Initial value for modify test',
+  'e2e_ws_rt_1770294960524' => 'WebSocket-Echtzeittestwert',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,8 +3,8 @@
 return [
   'e2e_basic_tr_1770294868553' => 'Hello from basic translation test',
   'e2e_modify_tr_1770294882433' => 'Modified value for modify test',
+  'e2e_ws_rt_1770294960524' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
-    'e2e_ws_rt_1770294960524' => 'WebSocket real-time test value',
 ];
