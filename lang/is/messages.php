2520cfde51c130dc9645d5984c6995ab5a165e3b
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:43:38 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770669274998' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770669344360' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
   'e2e_modify_tr_1770669289045' => 'Upphaflegt gildi fyrir breytingarpróf',
+  'e2e_pr_tr_1770669808960' => 'PR prófunargildi',
   'e2e_rt05_ws_1770669735391' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770669561016' => 'RT-21 próf fyrir notkunaruppfærslu',
   'e2e_rt24_ws_1770669425368' => 'RT-24 sjálfvirk áfyllingarpróf',
