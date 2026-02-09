3cd22173ad976264bbc12a08723da619d9efae0e
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:20:30 +0000

    Automatic translations

--- lang/es_US/messages.php
+++ lang/es_US/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770668368152' => 'Hola desde la prueba de traducción básica',
-  'e2e_delete_tr_1770668405122' => 'Valor a eliminar',
   'e2e_modify_tr_1770668382033' => 'Valor inicial para la prueba de modificación',
   'goodbye' => 'Adiós',
   'greeting' => 'Hola, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770668368152' => 'Hallo van basale vertalingstest',
-  'e2e_delete_tr_1770668405122' => 'Te verwijderen waarde',
   'e2e_modify_tr_1770668382033' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
