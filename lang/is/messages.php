9d97993e147a67e87639c6bae8c7c5367b7d9d99
LocaleFlow <noreply@localeflow.io>
2026-02-09 15:49:55 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770651902986' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770651973176' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
   'e2e_modify_tr_1770651917246' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_rt21_ws_1770652189873' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_rt24_ws_1770652055297' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770652011247' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
