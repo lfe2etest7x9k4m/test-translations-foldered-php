fe15c1b4d2709873f9157f606e218422225f5c67
LocaleFlow <noreply@localeflow.io>
2026-02-09 14:55:56 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770648948941' => 'Ahoj ze základního testu překladu',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770648948941' => 'Hallo vom grundlegenden Übersetzungstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen in unserer Anwendung',
