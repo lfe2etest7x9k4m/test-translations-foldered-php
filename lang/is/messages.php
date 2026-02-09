e6b750ada00c9eaa87318a3189c617925c026a1a
LocaleFlow <noreply@localeflow.io>
2026-02-09 21:38:51 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770672970476' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770673039993' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
   'e2e_modify_tr_1770672984466' => 'Upphafsgildi fyrir breytingapróf',
+  'e2e_rt24_ws_1770673126045' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770673077598' => 'WebSocket rauntíma prófgildi',
   'emoji_test' => 'Halló 🌍 Heimur 🎉',
   'goodbye' => 'Bless',
