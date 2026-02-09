6434c5449f96cef417489c19c01a6cce570b5fb1
LocaleFlow <noreply@localeflow.io>
2026-02-09 21:49:15 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770673719956' => 'Ahoj z testu základního překladu',
-  'e2e_modify_tr_1770673740784' => 'Beginwaarde voor wijzigingstest',
+  'e2e_modify_tr_1770673740784' => 'Počáteční hodnota pro test úprav',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
