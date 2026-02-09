084c1567c043987995a91bb2e3a2f18544243213
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:04:45 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770667455236' => 'Ahoj ze základního testu překladu',
-  'e2e_modify_tr_1770667469183' => 'Počáteční hodnota pro test úprav',
+  'e2e_modify_tr_1770667469183' => 'Initiële waarde voor wijzigingstest',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770667455236' => 'Hallo vom grundlegenden Übersetzungstest',
-  'e2e_modify_tr_1770667469183' => 'Initial value for modify test',
+  'e2e_modify_tr_1770667469183' => 'Geänderter Wert für den Änderungs-Test',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen in unserer Anwendung',
