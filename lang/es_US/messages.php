9ef98477cb7bd0de20a8ca25da46739e797b3c2a
LocaleFlow <noreply@localeflow.io>
2026-02-09 22:05:25 +0000

    Automatic translations

--- lang/es_US/messages.php
+++ lang/es_US/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770674571685' => 'Hello from basic translation test',
   'e2e_context_tr_1770674641220' => 'Por favor, utilice un lenguaje formal para este texto.',
   'e2e_modify_tr_1770674585607' => 'Valor inicial para la prueba de modificación',
+  'e2e_rt24_ws_1770674719281' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770674677568' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770674571685' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770674641220' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
   'e2e_modify_tr_1770674585607' => 'Upphaflegt gildi fyrir breytingarpróf',
+  'e2e_rt24_ws_1770674719281' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770674677568' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
