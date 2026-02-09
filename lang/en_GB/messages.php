b7395731cc82f703115515ce4bc334146c199483
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:44:40 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -9,8 +9,10 @@ return [
   'e2e_rt05_ws_1770669735391' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770669561016' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770669425368' => 'RT-24 auto top-up test',
+  'e2e_update_precedence_key' => 'UPDATED VALUE',
   'e2e_ws_rt_1770669382231' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
-  'welcome' => 'Welcome to our application',];
+  'welcome' => 'Welcome to our application',
+];
