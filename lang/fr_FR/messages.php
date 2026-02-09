afe7a5fa415a7252166fe9d7cd1102db25203f2a
LocaleFlow <noreply@localeflow.io>
2026-02-09 13:57:16 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770645384540' => 'Bonjour du test de traduction de base',
+  'e2e_delete_tr_1770645421735' => 'Valeur à supprimer',
   'e2e_modify_tr_1770645398435' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770645384540' => 'Halló frá grunnþýðingarprófi',
+  'e2e_delete_tr_1770645421735' => 'Gildi til að eyða',
   'e2e_modify_tr_1770645398435' => 'Upphafsgildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
