6e9d4947a97a65d0f517b21c017db96c611138f3
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-09 20:02:59 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770667034732' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1770667071714' => 'Gildi til að eyða',
-  'e2e_modify_tr_1770667048792' => 'Upphaflegt gildi fyrir breytingapróf',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770667034732' => 'Hallo van basale vertalingstest',
-  'e2e_delete_tr_1770667071714' => 'Te verwijderen waarde',
-  'e2e_modify_tr_1770667048792' => 'Initiële waarde voor wijzigingstest',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
