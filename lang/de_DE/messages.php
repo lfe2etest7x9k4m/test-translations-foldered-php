0f3a92da0d563b0e73a6c2d7b6567ab9a7d2265e
LocaleFlow <noreply@localeflow.io>
2026-02-09 21:57:31 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770673719956' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_context_tr_1770673795884' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1770673740784' => 'Anfangswert für Modifikationstest',
+  'e2e_quota_pause_1770674056716' => 'Kontingentpausentest',
   'e2e_rt21_ws_1770674010953' => 'RT-21-Nutzungsaktualisierungstest',
   'e2e_rt24_ws_1770673877112' => 'RT-24 automatische Aufladungstest',
   'e2e_ws_rt_1770673832896' => 'WebSocket-Echtzeittestwert',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770673719956' => 'Hello from basic translation test',
   'e2e_context_tr_1770673795884' => 'Please use formal language for this text',
   'e2e_modify_tr_1770673740784' => 'Initial value for modify test',
+  'e2e_quota_pause_1770674056716' => 'Quota pause test',
   'e2e_rt21_ws_1770674010953' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770673877112' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770673832896' => 'WebSocket real-time test value',
