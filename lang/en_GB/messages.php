a8c53930d9446594508da859172f38918c3bd66a
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 14:25:22 +0000

    E2E test: WebSocket RT test - Add e2e_ws_rt_1770647118059

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -7,4 +7,5 @@ return [
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
+    'e2e_ws_rt_1770647118059' => 'WebSocket real-time test value',
 ];
