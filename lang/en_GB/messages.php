e4500c0b6dbd04bdb2856e5d2266acdff56b3b41
LocaleFlow <noreply@localeflow.io>
2026-02-09 15:13:03 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -5,6 +5,7 @@ return [
   'e2e_context_tr_1770649519887' => 'Please use formal language for this text',
   'e2e_modify_tr_1770649457159' => 'Modified value for modify test',
   'e2e_quota_pause_1770649786607' => 'Quota pause test',
+  'e2e_rt05_ws_1770649913544' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770649736567' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770649601981' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770649557752' => 'WebSocket real-time test value',
