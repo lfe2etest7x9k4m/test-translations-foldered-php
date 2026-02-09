cf9e4d3062dd8362d52a0e9061832c362522e634
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 15:29:51 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,17 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770650248536' => 'Halló frá grunnþýðingarprófi',
-  'e2e_context_tr_1770650318469' => 'Vinsamlegast notið formlega tungu fyrir þennan texta',
-  'e2e_modify_tr_1770650262646' => 'Upphaflegt gildi fyrir breytingapróf',
-  'e2e_pr_tr_1770650791582' => 'PR prófunargildi',
-  'e2e_rt05_ws_1770650719497' => 'RT-05 WebSocket próf',
-  'e2e_rt21_ws_1770650542685' => 'RT-21 próf á notkunaruppfærslu',
-  'e2e_rt24_ws_1770650407941' => 'RT-24 sjálfvirk áfyllingarpróf',
-  'e2e_ws_rt_1770650363700' => 'WebSocket rauntíma prófunargildi',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
