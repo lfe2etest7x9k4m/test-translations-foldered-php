797448112070174e927c9d845e923bb6d30cf66c
LocaleFlow <noreply@localeflow.io>
2026-02-09 10:07:44 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770631636129' => 'Hallo von grundlegenden Übersetzungs Tests',
+  'e2e_modify_tr_1770631657220' => 'Anfangswert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
--- lang/es_US/messages.php
+++ lang/es_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770631636129' => 'Hola desde la prueba de traducción básica',
+  'e2e_modify_tr_1770631657220' => 'Valor inicial para la prueba de modificación',
   'goodbye' => 'Adiós',
   'greeting' => 'Hola, :name!',
   'welcome' => 'Bienvenido a nuestra aplicación',
