113bbb1ee28b99ffc1e62b15ee41628ffa058741
LocaleFlow <noreply@localeflow.io>
2026-02-09 23:47:56 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,10 +4,10 @@ return [
   'e2e_basic_tr_1770680590799' => 'Hello from basic translation test',
   'e2e_context_tr_1770680659432' => 'Please use formal language for this text',
   'e2e_modify_tr_1770680604357' => 'Modified value for modify test',
+  'e2e_rt21_ws_1770680870856' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770680737254' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770680695855' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
-    'e2e_rt21_ws_1770680870856' => 'RT-21 usage update test',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770680590799' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770680659432' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
   'e2e_modify_tr_1770680604357' => 'Upphaflegt gildi fyrir breytingapróf',
+  'e2e_rt21_ws_1770680870856' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_rt24_ws_1770680737254' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770680695855' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
