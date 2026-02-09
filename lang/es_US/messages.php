f9af0701fa442b6a7828895980149cf35bd099f7
LocaleFlow <noreply@localeflow.io>
2026-02-09 13:56:54 +0000

    Automatic translations

--- lang/es_US/messages.php
+++ lang/es_US/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770645384540' => 'Hola desde la prueba de traducción básica',
-  'e2e_modify_tr_1770645398435' => 'Valor inicial para la prueba de modificación',
+  'e2e_modify_tr_1770645398435' => 'Valor modificado para la prueba de modificación',
   'goodbye' => 'Adiós',
   'greeting' => 'Hola, :name!',
   'welcome' => 'Bienvenido a nuestra aplicación',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770645384540' => 'Hallo vanuit de basistest voor vertalingen',
-  'e2e_modify_tr_1770645398435' => 'Initiële waarde voor wijzigingstest',
+  'e2e_modify_tr_1770645398435' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
