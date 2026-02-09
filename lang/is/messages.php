49ba0419226c6613cda7bbfa02400986375c0b86
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 16:00:03 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,21 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770651902986' => 'Halló frá grunnþýðingarprófi',
-  'e2e_bp03_1770652569044' => 'GitHub verndunarpróf',
-  'e2e_context_tr_1770651973176' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
-  'e2e_err10_1770652543024' => 'Próf á höfnun á ýtingu',
-  'e2e_modify_tr_1770651917246' => 'Breytt gildi fyrir breytingapróf',
-  'e2e_multi_lang_1770652511951' => 'is original',
-  'e2e_pr_tr_1770652435502' => 'PR prófunargildi',
-  'e2e_quota_pause_1770652236638' => 'Próf á stöðvun kvóta',
-  'e2e_rt05_ws_1770652363548' => 'RT-05 WebSocket próf',
-  'e2e_rt21_ws_1770652189873' => 'RT-21 próf á notkunaruppfærslu',
-  'e2e_rt24_ws_1770652055297' => 'RT-24 sjálfvirk áfyllingarpróf',
-  'e2e_update_precedence_key' => 'is original',
-  'e2e_ws_rt_1770652011247' => 'WebSocket rauntíma prófunargildi',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,21 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770651902986' => 'Hallo van basale vertaaltest',
-  'e2e_bp03_1770652569044' => 'GitHub beschermingstest',
-  'e2e_context_tr_1770651973176' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_err10_1770652543024' => 'Push afwijzingstest',
-  'e2e_modify_tr_1770651917246' => 'Initiële waarde voor wijzigingstest',
-  'e2e_multi_lang_1770652511951' => 'Britse Engelse handmatige bewerking',
-  'e2e_pr_tr_1770652435502' => 'PR testwaarde',
-  'e2e_quota_pause_1770652236638' => 'Quota pauzetest',
-  'e2e_rt05_ws_1770652363548' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770652189873' => 'RT-21 gebruiksupdate test',
-  'e2e_rt24_ws_1770652055297' => 'RT-24 automatische opwaardeertest',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'e2e_ws_rt_1770652011247' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
