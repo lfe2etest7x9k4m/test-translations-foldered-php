10895cf9becfe16b0feb5cfd2fcf696837626734
LocaleFlow <noreply@localeflow.io>
2026-02-09 15:45:32 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770651902986' => 'Ahoj ze základního testu překladu',
-  'e2e_modify_tr_1770651917246' => 'Počáteční hodnota pro test úprav',
+  'e2e_modify_tr_1770651917246' => 'Upravená hodnota pro test úprav',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770651902986' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1770651917246' => 'Upphaflegt gildi fyrir breytingapróf',
+  'e2e_modify_tr_1770651917246' => 'Breytt gildi fyrir breytingapróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
