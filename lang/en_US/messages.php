402132f62243437deafb5d63209ee3289c9aa952
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:21:30 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770668368152' => 'Hello from basic translation test',
   'e2e_context_tr_1770668437529' => 'Please use formal language for this text',
   'e2e_modify_tr_1770668382033' => 'Initial value for modify test',
+  'e2e_ws_rt_1770668482107' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770668368152' => 'Bonjour du test de traduction de base',
   'e2e_context_tr_1770668437529' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_modify_tr_1770668382033' => 'Valeur modifiée pour le test de modification',
+  'e2e_ws_rt_1770668482107' => 'Valeur de test en temps réel WebSocket',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
