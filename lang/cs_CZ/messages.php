9900b4c3df2a6aa33a542a929a09562d9964a6b6
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:37:11 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770669274998' => 'Ahoj ze základního testu překladu',
   'e2e_context_tr_1770669344360' => 'Prosím, použijte pro tento text formální jazyk.',
   'e2e_modify_tr_1770669289045' => 'Počáteční hodnota pro test úprav',
+  'e2e_rt24_ws_1770669425368' => 'RT-24 automatické doplňování test',
   'e2e_ws_rt_1770669382231' => 'Hodnota testu v reálném čase WebSocket',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770669274998' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770669344360' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
   'e2e_modify_tr_1770669289045' => 'Upphaflegt gildi fyrir breytingarpróf',
+  'e2e_rt24_ws_1770669425368' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770669382231' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
