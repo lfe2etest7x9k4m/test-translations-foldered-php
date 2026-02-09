8ff53cc9d8bd2720b69431b15df95510e98dc82d
LocaleFlow <noreply@localeflow.io>
2026-02-09 18:26:38 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770660984286' => 'Bonjour du test de traduction de base',
   'e2e_context_tr_1770661089715' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_modify_tr_1770661006169' => 'Valeur modifiée pour le test de modification',
+  'e2e_pr_tr_1770661587186' => 'Valeur de test PR',
   'e2e_rt05_ws_1770661509103' => 'Test WebSocket RT-05',
   'e2e_rt21_ws_1770661331767' => 'Test de mise à jour d\'utilisation RT-21',
   'e2e_rt24_ws_1770661196622' => 'Test de recharge automatique RT-24',
