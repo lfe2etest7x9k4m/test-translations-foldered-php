f3e47d691ef8f744ecf7cb9e61eb970aee7714e8
LocaleFlow <noreply@localeflow.io>
2026-02-09 14:01:50 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770645648550' => 'Hallo von grundlegendem Übersetzungstest',
-  'e2e_delete_tr_1770645685999' => 'Zu löschender Wert',
   'e2e_modify_tr_1770645662452' => 'Anfangswert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770645648550' => 'Hallo vanuit de basale vertaaltest',
-  'e2e_delete_tr_1770645685999' => 'Waarde om te verwijderen',
   'e2e_modify_tr_1770645662452' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
