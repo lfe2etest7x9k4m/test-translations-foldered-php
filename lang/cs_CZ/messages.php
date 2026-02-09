9326bb8b855bdff496139a718d54ed546fd99e4d
LocaleFlow <noreply@localeflow.io>
2026-02-09 15:40:53 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -8,7 +8,7 @@ return [
   'e2e_rt05_ws_1770651530250' => 'RT-05 test WebSocket',
   'e2e_rt21_ws_1770651353763' => 'RT-21 test aktualizace využití',
   'e2e_rt24_ws_1770651218990' => 'RT-24 automatické doplňování',
-  'e2e_update_precedence_key' => 'is original',
+  'e2e_update_precedence_key' => 'UPDATED VALUE',
   'e2e_ws_rt_1770651174875' => 'Hodnota testu v reálném čase WebSocket',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
