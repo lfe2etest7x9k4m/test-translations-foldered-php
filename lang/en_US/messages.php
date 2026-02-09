e21faa2cf146dad57a4c9bee061f4e5e73634596
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 14:15:59 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770646404802' => 'Hello from basic translation test',
-  'e2e_context_tr_1770646474531' => 'Please use formal language for this text',
-  'e2e_modify_tr_1770646418694' => 'Initial value for modify test',
-  'e2e_ws_rt_1770646511987' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770646404802' => 'Halló frá grunnþýðingarprófi',
-  'e2e_context_tr_1770646474531' => 'Vinsamlegast notið formlega tungu fyrir þennan texta',
-  'e2e_modify_tr_1770646418694' => 'Upphafsgildi fyrir breytingarpróf',
-  'e2e_ws_rt_1770646511987' => 'WebSocket rauntíma prófgildi',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
