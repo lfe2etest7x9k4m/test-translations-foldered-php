2b653ea09fcf780711674ab0efe5fb1a5b0eac9f
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-09 22:01:36 +0000

    Reset test repo to clean state

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,17 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770673719956' => 'Hallo van basale vertaaltest',
-  'e2e_context_tr_1770673795884' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_modify_tr_1770673740784' => 'Initiële waarde voor wijzigingstest',
-  'e2e_pr_tr_1770674257288' => 'PR testwaarde',
-  'e2e_rt05_ws_1770674182830' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770674010953' => 'RT-21 gebruiksupdate test',
-  'e2e_rt24_ws_1770673877112' => 'RT-24 automatische bijvultest',
-  'e2e_ws_rt_1770673832896' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_update_precedence_key' => 'nl_NL original',
 ];
