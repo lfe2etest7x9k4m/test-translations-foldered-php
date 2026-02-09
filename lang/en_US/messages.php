fed608861dd3743123bd11efce43d3739e87d313
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 15:55:26 +0000

    Test EDGE-02: Modify same key in en_GB and en_US

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1770651902986' => 'Hello from basic translation test',
   'e2e_context_tr_1770651973176' => 'Please use formal language for this text',
   'e2e_modify_tr_1770651917246' => 'Initial value for modify test',
-  'e2e_multi_lang_1770652511951' => 'en_US original',
+  'e2e_multi_lang_1770652511951' => 'American English manual edit',
   'e2e_pr_tr_1770652435502' => 'PR test value',
   'e2e_quota_pause_1770652236638' => 'Quota pause test',
   'e2e_rt05_ws_1770652363548' => 'RT-05 WebSocket test',
