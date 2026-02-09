da5b65e123a607bb3cfdc606652be80c8c88d7a9
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:49:05 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770670135854' => 'Ahoj ze základního testu překladu',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
