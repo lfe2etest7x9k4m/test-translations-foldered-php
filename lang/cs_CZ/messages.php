c30b1b40bbc6f58c2a7e53fc7a3b998282c95d8d
LocaleFlow <noreply@localeflow.io>
2026-02-09 10:08:25 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770631636129' => 'Ahoj ze základního testu překladu',
-  'e2e_delete_tr_1770631680333' => 'Hodnota k odstranění',
   'e2e_modify_tr_1770631657220' => 'Počáteční hodnota pro test úprav',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Dobrý den, :name!',
