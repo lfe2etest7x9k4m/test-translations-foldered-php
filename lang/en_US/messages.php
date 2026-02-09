abdde2d7a229542a0d975eb3750cc03933eb88e0
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 18:14:58 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,15 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770652929481' => 'Hello from basic translation test',
-  'e2e_context_tr_1770652999584' => 'Please use formal language for this text',
-  'e2e_modify_tr_1770652943631' => 'Initial value for modify test',
-  'e2e_rt05_ws_1770653393904' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770653217253' => 'RT-21 usage update test',
-  'e2e_rt24_ws_1770653082592' => 'RT-24 auto top-up test',
-  'e2e_ws_rt_1770653037352' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,15 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770652929481' => 'Hallo van basale vertalingstest',
-  'e2e_context_tr_1770652999584' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_modify_tr_1770652943631' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_rt05_ws_1770653393904' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770653217253' => 'RT-21 gebruiksupdate test',
-  'e2e_rt24_ws_1770653082592' => 'RT-24 automatische opwaardeertest',
-  'e2e_ws_rt_1770653037352' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
