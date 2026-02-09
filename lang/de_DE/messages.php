79fab410607159f09f9364141c0b789de927382f
LocaleFlow <noreply@localeflow.io>
2026-02-09 20:19:49 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770668368152' => 'Hallo von grundlegenden Übersetzungstests',
+  'e2e_modify_tr_1770668382033' => 'Initial value for modify test',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
--- lang/es_US/messages.php
+++ lang/es_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770668368152' => 'Hola desde la prueba de traducción básica',
+  'e2e_modify_tr_1770668382033' => 'Valor inicial para la prueba de modificación',
   'goodbye' => 'Adiós',
   'greeting' => 'Hola, :name!',
   'welcome' => 'Bienvenido a nuestra aplicación',
