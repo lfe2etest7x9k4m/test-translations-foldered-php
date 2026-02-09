be6c7cf62384f52320190321e1fb6d0095b4b59c
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 10:06:01 +0000

    Reset test repo to clean state

--- lang/de_DE/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-  'e2e_basic_tr_1770631403735' => 'Hallo von grundlegenden Übersetzungs Tests',
-  'e2e_context_tr_1770631473150' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
-  'e2e_modify_tr_1770631417662' => 'Anfangswert für Modifikationstest',
-  'goodbye' => 'Auf Wiedersehen',
-  'welcome' => 'Willkommen bei unserer Anwendung',
-];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770631403735' => 'Hallo van basale vertalingstest',
-  'e2e_context_tr_1770631473150' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_modify_tr_1770631417662' => 'Initiële waarde voor wijzigingstest',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
