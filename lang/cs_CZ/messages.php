38212d7277d254b6fbe22b74a763eacede8a3d70
LocaleFlow <noreply@localeflow.io>
2026-02-09 10:07:23 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770631636129' => 'Ahoj ze základního testu překladu',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Dobrý den, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770631636129' => 'Halló frá grunnþýðingarprófi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
