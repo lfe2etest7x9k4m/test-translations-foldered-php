35e3dfdb556a8f52b152259f9bc6444daf3657d8
LocaleFlow <noreply@localeflow.io>
2026-02-09 21:29:24 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770672012455' => 'Hello from basic translation test',
   'e2e_context_tr_1770672082136' => 'Please use formal language for this text',
   'e2e_modify_tr_1770672026441' => 'Modified value for modify test',
+  'e2e_pr_tr_1770672558350' => 'PR test value',
   'e2e_quota_pause_1770672358352' => 'Quota pause test',
   'e2e_rt05_ws_1770672485090' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770672305983' => 'RT-21 usage update test',
@@ -13,5 +14,4 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_pr_tr_1770672558350' => 'PR test value',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770672012455' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770672082136' => 'Vinsamlegast notið formlegt mál fyrir þennan texta',
   'e2e_modify_tr_1770672026441' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_pr_tr_1770672558350' => 'PR prófunargildi',
   'e2e_rt05_ws_1770672485090' => 'RT-05 WebSocket próf',
   'e2e_rt21_ws_1770672305983' => 'RT-21 próf fyrir notkunaruppfærslu',
   'e2e_rt24_ws_1770672171455' => 'RT-24 sjálfvirk áfyllingarpróf',
