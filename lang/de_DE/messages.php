ab0375aa22fe31bc468a97a327cbbb6b5b124cec
LocaleFlow <noreply@localeflow.io>
2026-02-09 18:16:36 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770660984286' => 'Hallo von grundlegenden Übersetzungstests',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
