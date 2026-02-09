24b39edaea9bd6c9c26c6d6879d57b9b65beb451
LocaleFlow <noreply@localeflow.io>
2026-02-09 21:31:05 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770672012455' => 'Hello from basic translation test',
   'e2e_context_tr_1770672082136' => 'Please use formal language for this text',
+  'e2e_err10_1770672657480' => 'Push rejection test',
   'e2e_modify_tr_1770672026441' => 'Modified value for modify test',
   'e2e_multi_lang_1770672627220' => 'British English manual edit',
   'e2e_pr_tr_1770672558350' => 'PR test value',
@@ -16,5 +17,4 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_err10_1770672657480' => 'Push rejection test',
 ];
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770672012455' => 'Hello from basic translation test',
   'e2e_context_tr_1770672082136' => 'Please use formal language for this text',
+  'e2e_err10_1770672657480' => 'Push rejection test',
   'e2e_modify_tr_1770672026441' => 'Initial value for modify test',
   'e2e_multi_lang_1770672627220' => 'American English manual edit',
   'e2e_pr_tr_1770672558350' => 'PR test value',
