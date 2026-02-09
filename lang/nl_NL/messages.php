64a7f1988a89b7e2bfedcef30602577e74ff98de
LocaleFlow <noreply@localeflow.io>
2026-02-09 15:09:36 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -9,5 +9,6 @@ return [
   'e2e_ws_rt_1770649557752' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
 ];
