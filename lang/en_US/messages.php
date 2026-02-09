cd504b212220c2959be14ead359b1f55cafb3ba2
LocaleFlow <noreply@localeflow.io>
2026-02-09 15:53:49 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770651902986' => 'Hello from basic translation test',
   'e2e_context_tr_1770651973176' => 'Please use formal language for this text',
   'e2e_modify_tr_1770651917246' => 'Initial value for modify test',
+  'e2e_quota_pause_1770652236638' => 'Quota pause test',
   'e2e_rt21_ws_1770652189873' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770652055297' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770652011247' => 'WebSocket real-time test value',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770651902986' => 'Hallo van basale vertaaltest',
   'e2e_context_tr_1770651973176' => 'Gelieve formele taal te gebruiken voor deze tekst',
   'e2e_modify_tr_1770651917246' => 'Initiële waarde voor wijzigingstest',
+  'e2e_quota_pause_1770652236638' => 'Quota pauzetest',
   'e2e_rt21_ws_1770652189873' => 'RT-21 gebruiksupdate test',
   'e2e_rt24_ws_1770652055297' => 'RT-24 automatische opwaardeertest',
   'e2e_ws_rt_1770652011247' => 'WebSocket real-time testwaarde',
