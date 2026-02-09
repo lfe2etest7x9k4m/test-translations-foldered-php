ee5ba24e27cdf8bbf95605ce2d9b5a4a24bdff90
LocaleFlow <noreply@localeflow.io>
2026-02-09 21:57:44 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770673719956' => 'Ahoj z testu základního překladu',
   'e2e_context_tr_1770673795884' => 'Prosím, použijte pro tento text formální jazyk.',
   'e2e_modify_tr_1770673740784' => 'Počáteční hodnota pro test úprav',
+  'e2e_pr_tr_1770674257288' => 'Hodnota testu PR',
   'e2e_rt05_ws_1770674182830' => 'RT-05 test WebSocket',
   'e2e_rt21_ws_1770674010953' => 'Test aktualizace použití RT-21',
   'e2e_rt24_ws_1770673877112' => 'RT-24 automatické doplnění test',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770673719956' => 'Hello from basic translation test',
   'e2e_context_tr_1770673795884' => 'Please use formal language for this text',
   'e2e_modify_tr_1770673740784' => 'Initial value for modify test',
+  'e2e_pr_tr_1770674257288' => 'PR test value',
   'e2e_rt05_ws_1770674182830' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770674010953' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770673877112' => 'RT-24 auto top-up test',
