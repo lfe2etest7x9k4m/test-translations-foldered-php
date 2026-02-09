764c0869c6f194fc2ef3a3dc1c08b53f0eafe487
lfe2etest7x9k4m <test@colinsouth.com>
2026-02-09 14:30:20 +0000

    Reset test repo to clean state

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770646996303' => 'Hallo van basale vertaaltest',
-  'e2e_context_tr_1770647065882' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_modify_tr_1770647010292' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_ws_rt_1770647118059' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
