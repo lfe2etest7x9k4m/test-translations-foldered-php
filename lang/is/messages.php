f18d3b49419452ef9f8e00ede1e07311a954454e
LocaleFlow <noreply@localeflow.io>
2026-02-10 00:41:54 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770683831002' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1770683899725' => 'Vinsamlegast notið formlega tungu fyrir þennan texta',
   'e2e_modify_tr_1770683844515' => 'Breytt gildi fyrir breytingapróf',
+  'e2e_rt21_ws_1770684109655' => 'RT-21 notkunaruppfærslupróf',
   'e2e_rt24_ws_1770683976122' => 'RT-24 sjálfvirk áfyllingarpróf',
   'e2e_ws_rt_1770683935870' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
