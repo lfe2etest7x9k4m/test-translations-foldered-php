845b41ad4a17b9c6834464945cc521d726c8785b
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-11 17:43:13 +0000

    E2E test: WebSocket RT test - Add e2e_ws_rt_1770831789237

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -7,4 +7,5 @@ return [
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
+    'e2e_ws_rt_1770831789237' => 'WebSocket real-time test value',
 ];
