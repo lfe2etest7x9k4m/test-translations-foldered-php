3f79edaa0f18dc9181435c751c503b0671600ac6
LocaleFlow <noreply@localeflow.io>
2026-02-11 17:20:54 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770830117637' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1770830207933' => 'Vinsamlegast notið formlegt orðalag fyrir þennan texta',
   'e2e_modify_tr_1770830138322' => 'Breytt gildi fyrir breytingarprófun',
+  'e2e_rt21_ws_1770830444008' => 'RT-21 prófun á notkunaruppfærslu',
   'e2e_rt24_ws_1770830310296' => 'RT-24 prófun á sjálfvirkri viðbót',
   'e2e_ws_rt_1770830265998' => 'Prófunargildi fyrir rauntíma WebSocket',
   'goodbye' => 'Bless',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770830117637' => 'Hallo vanuit een eenvoudige vertaaltest',
   'e2e_context_tr_1770830207933' => 'Gelieve formele taal te gebruiken voor deze tekst',
   'e2e_modify_tr_1770830138322' => 'Gewijzigde waarde voor test van de wijziging',
+  'e2e_rt21_ws_1770830444008' => 'Test van de update voor RT-21-gebruik',
   'e2e_rt24_ws_1770830310296' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770830265998' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
