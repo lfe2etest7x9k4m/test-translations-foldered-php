9a23708df72e742d0f1c7ac8063083c0a653b8d0
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-11 18:10:44 +0000

    Test RT-05: e2e_rt05_ws_1770833443865

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -12,4 +12,5 @@ return [
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
     'e2e_quota_pause_1770833318944' => 'Quota pause test',
+    'e2e_rt05_ws_1770833443865' => 'RT-05 WebSocket test',
 ];
