9655497a86df0ac1db9e2596a03a7c9fd82b7fc0
LocaleFlow <noreply@localeflow.io>
2026-02-11 22:29:21 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770848894743' => 'Halló frá almennri þýðingarprófi',
+  'e2e_delete_tr_1770848946598' => 'Gildi til að eyða',
   'e2e_modify_tr_1770848913422' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770848894743' => 'Hallo vanuit basisvertalingstest',
+  'e2e_delete_tr_1770848946598' => 'Te verwijderen waarde',
   'e2e_modify_tr_1770848913422' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
