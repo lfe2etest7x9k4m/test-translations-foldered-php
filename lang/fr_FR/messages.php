60f42634c9fb532e6264e2047f5c7134c652fd60
LocaleFlow <noreply@localeflow.io>
2026-02-11 17:27:28 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1770830117637' => 'Bonjour, provenant du test de traduction de base',
   'e2e_context_tr_1770830207933' => '#DO_NOT_TRANSLATE',
   'e2e_modify_tr_1770830138322' => 'Valeur modifiée pour le test',
-  'e2e_multi_lang_1770830816961' => 'en_GB original',
+  'e2e_multi_lang_1770830816961' => 'Modification manuelle en anglais britannique',
   'e2e_pr_tr_1770830691905' => 'Valeur de test PR',
   'e2e_quota_pause_1770830495398' => 'Test de pause de quota',
   'e2e_rt21_ws_1770830444008' => 'Test de mise à jour d\'utilisation RT-21',
