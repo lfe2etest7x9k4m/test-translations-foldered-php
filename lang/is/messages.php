587c7b1edce2c48391340adc8a9af2bc8a5a6dbe
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-11 17:27:01 +0000

    Setup EDGE-02: Add e2e_multi_lang_1770830816961

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -14,4 +14,5 @@ return [
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Velkomin í forritið okkar',
+    'e2e_multi_lang_1770830816961' => 'is original',
 ];
