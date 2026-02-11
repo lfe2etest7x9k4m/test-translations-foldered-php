4017d60469855ba6d9ede53b78b43c22e64be2f2
LocaleFlow <noreply@localeflow.io>
2026-02-11 17:25:32 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1770830117637' => 'Hallo von einem grundlegenden Übersetzungstest',
   'e2e_context_tr_1770830207933' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache.',
   'e2e_modify_tr_1770830138322' => 'Geänderter Wert für Änderungstest',
-  'e2e_quota_pause_1770830495398' => 'Quotenunterbrechungstest',
+  'e2e_rt05_ws_1770830620273' => 'RT-05 WebSocket-Test',
   'e2e_rt21_ws_1770830444008' => 'RT-21 Nutzungs-Update-Test',
   'e2e_rt24_ws_1770830310296' => 'RT-24 automatischer Aufladungstest',
   'e2e_ws_rt_1770830265998' => 'WebSocket Echtzeit-Testwert',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1770830117637' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1770830207933' => 'Vinsamlegast notið formlegt orðalag fyrir þennan texta',
   'e2e_modify_tr_1770830138322' => 'Breytt gildi fyrir breytingarprófun',
-  'e2e_quota_pause_1770830495398' => 'Prófun á kvótafresti',
+  'e2e_rt05_ws_1770830620273' => 'RT-05 WebSocket prófun',
   'e2e_rt21_ws_1770830444008' => 'RT-21 prófun á notkunaruppfærslu',
   'e2e_rt24_ws_1770830310296' => 'RT-24 prófun á sjálfvirkri viðbót',
   'e2e_ws_rt_1770830265998' => 'Prófunargildi fyrir rauntíma WebSocket',
