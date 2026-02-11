d205a5bea72610d63a45e2379705f969a282c50a
LocaleFlow <noreply@localeflow.io>
2026-02-11 17:24:50 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770830117637' => 'Bonjour, provenant du test de traduction de base',
   'e2e_context_tr_1770830207933' => '#DO_NOT_TRANSLATE',
   'e2e_modify_tr_1770830138322' => 'Valeur modifiée pour le test',
+  'e2e_quota_pause_1770830495398' => 'Test de pause de quota',
   'e2e_rt21_ws_1770830444008' => 'Test de mise à jour d\'utilisation RT-21',
   'e2e_rt24_ws_1770830310296' => 'Test de recharge automatique RT-24',
   'e2e_ws_rt_1770830265998' => 'Valeur de test en temps réel WebSocket',
