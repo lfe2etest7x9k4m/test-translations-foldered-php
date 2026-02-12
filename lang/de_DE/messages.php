95350d01f22555f9bbd4508b7ab7cd15c331b17e
LocaleFlow <noreply@localeflow.io>
2026-02-12 21:55:49 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770933305691' => 'Hallo von einem einfachen Übersetzungstest',
-  'e2e_modify_tr_1770933324377' => 'Anfangswert für Modifikationstest',
+  'e2e_modify_tr_1770933324377' => 'Geänderter Wert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen in unserer App',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770933305691' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_modify_tr_1770933324377' => 'Upphaflegt gildi fyrir prófun á breytingar',
+  'e2e_modify_tr_1770933324377' => 'Breytt gildi í prófun á breytingum',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
