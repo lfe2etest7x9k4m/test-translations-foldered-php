867c2354b70021ff6e16ca6fc24f5a90b9e365af
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-12 10:29:52 +0000

    E2E test: Modify e2e_modify_tr_1770892177567 in en_GB

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770892158637' => 'Hello from basic translation test',
-  'e2e_modify_tr_1770892177567' => 'Initial value for modify test',
+  'e2e_modify_tr_1770892177567' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
