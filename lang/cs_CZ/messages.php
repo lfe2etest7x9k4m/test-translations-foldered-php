991043dd407495b2547ed5085ca7d334a4c08272
LocaleFlow <noreply@localeflow.io>
2026-02-12 21:17:18 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770930881443' => 'Ahoj z testu základního překladu',
   'e2e_context_tr_1770930965199' => 'Prosím, použijte pro tento text formální jazyk',
   'e2e_modify_tr_1770930900075' => 'Upravená hodnota pro testování změn',
+  'e2e_ws_rt_1770931026750' => 'Testovací hodnota WebSocket v reálném čase',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
