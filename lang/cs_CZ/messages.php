b4515ffdc04f53fac6f1707f2a5d362c8282a8d8
LocaleFlow <noreply@localeflow.io>
2026-02-12 22:37:33 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770935776830' => 'Dobrý den z testu základního překladu',
-  'e2e_delete_tr_1770935828621' => 'Hodnota k odstranění',
   'e2e_modify_tr_1770935795628' => 'Upravená hodnota pro testování úpravy',
   'goodbye' => 'Ahoj',
   'greeting' => 'Ahoj, :name!',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770935776830' => 'Bonjour depuis le test de traduction simple',
-  'e2e_delete_tr_1770935828621' => 'Valeur à supprimer',
   'e2e_modify_tr_1770935795628' => 'Valeur modifiée pour le test de modifier',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour :name !',
