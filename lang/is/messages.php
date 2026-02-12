041fac2a6c0c661e5143ad23591fb325bd752090
LocaleFlow <noreply@localeflow.io>
2026-02-12 14:27:51 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770905866691' => 'Halló frá prófi í einfaldri þýðingu',
   'e2e_context_tr_1770905950911' => 'Vinsamlegast notið formlegt tungutak fyrir þennan texta',
   'e2e_modify_tr_1770905885368' => 'Breytt gildi fyrir prófun á breytingu',
+  'e2e_multi_lang_1770906454299' => 'is original',
   'e2e_pr_tr_1770906364760' => 'PR prófunargildi',
   'e2e_quota_pause_1770906265008' => 'Kvóta bið prófun',
   'e2e_rt05_ws_1770906276985' => 'RT-05 WebSocket-prófun',
@@ -15,5 +16,4 @@ return [
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Prófunargildi',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_multi_lang_1770906454299' => 'is original',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770905866691' => 'Hallo vanuit de basisvertalingstest',
   'e2e_context_tr_1770905950911' => 'Gebruik alstublieft formele taal voor deze tekst',
   'e2e_modify_tr_1770905885368' => 'Gewijzigde waarde voor de test van het wijzigen',
+  'e2e_multi_lang_1770906454299' => 'nl_NL original',
   'e2e_pr_tr_1770906364760' => 'PR-testwaarde',
   'e2e_quota_pause_1770906265008' => 'Quota pauze test',
   'e2e_rt05_ws_1770906276985' => 'RT-05 WebSocket test',
@@ -15,5 +16,4 @@ return [
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_multi_lang_1770906454299' => 'nl_NL original',
 ];
