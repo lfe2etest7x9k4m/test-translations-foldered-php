ef5a63bc2471ca8dc70de506a1a4c7865a20b37e
LocaleFlow <noreply@localeflow.io>
2026-02-12 11:25:33 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770895375839' => 'Hallo aus dem grundlegenden Übersetzungstest',
   'e2e_context_tr_1770895459392' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1770895394411' => 'Geänderter Wert für Test zur Änderung',
+  'e2e_ws_rt_1770895521076' => 'Testwert für WebSocket-Echtzeit',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen zu unserer Anwendung',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770895375839' => 'Halló frá prófun á grunnþýðingu',
   'e2e_context_tr_1770895459392' => 'Vinsamlegast notið formála tungutak í þessum texta',
   'e2e_modify_tr_1770895394411' => 'Breytt gildi fyrir prófun á breytingum',
+  'e2e_ws_rt_1770895521076' => 'Prófunargildi fyrir WebSocket í rauntíma',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
