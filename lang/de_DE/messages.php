a642557ffcde87a1aa97b80e30d488b4087d3aae
LocaleFlow <noreply@localeflow.io>
2026-02-12 14:11:23 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770905258532' => 'Hallo aus dem grundlegenden Übersetzungstest',
   'e2e_context_tr_1770905349030' => 'Der Ton dieses Textes sollte formell sein.',
   'e2e_modify_tr_1770905283212' => 'Geänderter Wert für Modifikationstest',
+  'e2e_rt24_ws_1770905471138' => 'RT-24 automatische Aufladungstest',
   'e2e_ws_rt_1770905410786' => 'WebSocket Echtzeit-Testwert',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
