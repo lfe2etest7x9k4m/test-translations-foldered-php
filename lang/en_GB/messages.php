9a5e1d7ffd4b0d5b29c19fbc9d2fcb4bfe4423e5
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-12 11:21:36 +0000

    Reset test repo to clean state

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,18 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770892158637' => 'Hello from basic translation test',
-  'e2e_context_tr_1770892242659' => 'Please use formal language for this text',
-  'e2e_modify_tr_1770892177567' => 'Modified value for modify test',
-  'e2e_multi_lang_1770892669051' => 'British English manual edit',
-  'e2e_quota_pause_1770892532737' => 'Quota pause test',
-  'e2e_rt21_ws_1770892484367' => 'RT-21 usage update test',
-  'e2e_rt24_ws_1770892350132' => 'RT-24 auto top-up test',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'e2e_ws_rt_1770892304220' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
-    'e2e_err10_1770892721466' => 'Push rejection test',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,17 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770892158637' => 'Halló frá grunnþýðingarprófi',
-  'e2e_context_tr_1770892242659' => 'Vinsamlegast notið formlegt tungutak í þessum texta',
-  'e2e_modify_tr_1770892177567' => 'Breytt gildi fyrir breytingarprófun',
-  'e2e_multi_lang_1770892669051' => 'Handvirk breyting á breskri ensku',
-  'e2e_quota_pause_1770892532737' => 'Próf á biðkvóta',
-  'e2e_rt21_ws_1770892484367' => 'RT-21 prófun á uppfærslu á notkun',
-  'e2e_rt24_ws_1770892350132' => 'RT-24 sjálfvirk áfyllingarathugun',
-  'e2e_update_precedence_key' => 'is original',
-  'e2e_ws_rt_1770892304220' => 'Prófunargildi fyrir WebSocket í rauntíma',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
