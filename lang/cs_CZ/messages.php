fe921f333447cdc1c9cede43ee006f453417db82
LocaleFlow <noreply@localeflow.io>
2026-02-12 21:42:55 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -9,5 +9,6 @@ return [
   'e2e_ws_rt_1770932330234' => 'Hodnota testu WebSocket v reálném čase',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
+  'quota_test_key' => 'Testovací hodnota',
   'welcome' => 'Vítejte v naší aplikaci',
 ];
