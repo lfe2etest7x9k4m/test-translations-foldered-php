e3108922570e17f5aeaf6dceec39097df3aae303
LocaleFlow <noreply@localeflow.io>
2026-02-12 22:39:58 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770935776830' => 'Dobrý den z testu základního překladu',
   'e2e_context_tr_1770935860950' => 'Prosím, použijte pro tento text formální jazyk',
   'e2e_modify_tr_1770935795628' => 'Upravená hodnota pro testování úpravy',
+  'e2e_rt24_ws_1770935983970' => 'RT-24 test automatického doplnění',
   'e2e_ws_rt_1770935922998' => 'Testovací hodnota v reálném čase pro WebSocket',
   'goodbye' => 'Ahoj',
   'greeting' => 'Ahoj, :name!',
