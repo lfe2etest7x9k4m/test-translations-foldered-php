03bdaf4ccabad232d09924b96dad26086b5db352
LocaleFlow <noreply@localeflow.io>
2026-02-12 23:07:00 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770937549973' => 'Zdravím z testu základního překladu',
+  'e2e_delete_tr_1770937601577' => 'Hodnota k odstranění',
   'e2e_modify_tr_1770937568565' => 'Upravená hodnota pro test úpravy',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770937549973' => 'Bonjour depuis le test de traduction basique',
+  'e2e_delete_tr_1770937601577' => 'Valeur à supprimer',
   'e2e_modify_tr_1770937568565' => 'Valeur modifiée pour tester la modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
