67d4c41a07ea45e046541e4bbd35810ad7b8e50e
LocaleFlow <noreply@localeflow.io>
2026-02-12 21:55:17 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770933305691' => 'Zdravím z testu základního překladu',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770933305691' => 'Hallo von einem einfachen Übersetzungstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen in unserer App',
