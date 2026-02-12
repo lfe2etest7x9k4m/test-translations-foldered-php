f7f6e0f912101a29fa19ef603b920cfba1855d8e
LocaleFlow <noreply@localeflow.io>
2026-02-12 09:48:32 +0000

    Automatic translations

--- lang/es_US/messages.php
+++ lang/es_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770889645325' => 'Hola desde la prueba de traducción básica',
+  'e2e_delete_tr_1770889697060' => 'Valor a eliminar',
   'e2e_modify_tr_1770889663876' => 'Valor modificado para la prueba de edición',
   'goodbye' => 'Adiós',
   'greeting' => 'Hola, :name!',
