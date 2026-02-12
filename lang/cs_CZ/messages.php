84af6ab037ea2bce5bea36235101e3bb12a2b3dd
LocaleFlow <noreply@localeflow.io>
2026-02-12 10:32:41 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770892158637' => 'Ahoj z testu základního překladu',
   'e2e_context_tr_1770892242659' => 'Prosím, použijte pro tento text formální jazyk',
   'e2e_modify_tr_1770892177567' => 'Upravená hodnota pro test úpravy',
+  'e2e_rt24_ws_1770892350132' => 'Test automatického doplnění RT-24',
   'e2e_ws_rt_1770892304220' => 'Testovací hodnota WebSocket v reálném čase',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770892158637' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_context_tr_1770892242659' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1770892177567' => 'Geänderter Wert für den Test der Modifikation',
+  'e2e_rt24_ws_1770892350132' => 'RT-24 automatische Aufladungstest',
   'e2e_ws_rt_1770892304220' => 'WebSocket-Echtzeit-Testwert',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
