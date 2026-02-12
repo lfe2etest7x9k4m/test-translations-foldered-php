07e1e14111095415d5661082b9d925d3efd02636
LocaleFlow <noreply@localeflow.io>
2026-02-12 21:25:03 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770930881443' => 'Bonjour du test de traduction simple',
   'e2e_context_tr_1770930965199' => 'Veuillez utiliser un langage formel pour ce texte',
+  'e2e_err10_1770931491385' => 'Test de rejet de push',
   'e2e_modify_tr_1770930900075' => 'Valeur modifiée pour le test de modification',
   'e2e_multi_lang_1770931429270' => 'Modification manuelle en anglais britannique',
   'e2e_pr_tr_1770931340854' => 'Valeur de test de la pull request',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770930881443' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1770930965199' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
+  'e2e_err10_1770931491385' => 'Prófun á höfnun við ýtingu',
   'e2e_modify_tr_1770930900075' => 'Breytt gildi fyrir breytingarprófun',
   'e2e_multi_lang_1770931429270' => 'Handvirk breyting á breskri ensku',
   'e2e_pr_tr_1770931340854' => 'Gildi fyrir PR-próf',
