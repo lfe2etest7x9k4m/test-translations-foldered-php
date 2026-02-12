58b44140bc7962b86598950f453c838e6fd14c80
LocaleFlow <noreply@localeflow.io>
2026-02-12 23:15:05 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770937549973' => 'Bonjour depuis le test de traduction basique',
   'e2e_context_tr_1770937633743' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_modify_tr_1770937568565' => 'Valeur modifiée pour tester la modification',
+  'e2e_multi_lang_1770938088673' => 'fr_FR original',
   'e2e_pr_tr_1770938000612' => 'Valeur de test PR',
   'e2e_quota_pause_1770937944783' => 'Test de pause du quota',
   'e2e_rt05_ws_1770937956608' => 'Test WebSocket RT-05',
@@ -15,5 +16,4 @@ return [
   'greeting' => 'Bonjour, :name !',
   'quota_test_key' => 'Valeur test',
   'welcome' => 'Bienvenue dans notre application',
-    'e2e_multi_lang_1770938088673' => 'fr_FR original',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770937549973' => 'Hallo vanuit de basisvertalingstest',
   'e2e_context_tr_1770937633743' => 'Gelieve formele taal te gebruiken voor deze tekst',
   'e2e_modify_tr_1770937568565' => 'Gewijzigde waarde voor wijzigingstest',
+  'e2e_multi_lang_1770938088673' => 'nl_NL original',
   'e2e_pr_tr_1770938000612' => 'PR testwaarde',
   'e2e_quota_pause_1770937944783' => 'Quota pause test',
   'e2e_rt05_ws_1770937956608' => 'RT-05 WebSocket test',
@@ -15,5 +16,4 @@ return [
   'greeting' => 'Hallo, :name!',
   'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_multi_lang_1770938088673' => 'nl_NL original',
 ];
