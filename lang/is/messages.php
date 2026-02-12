5f7b49548f158ca64970595e1c1d0d4d89bc4680
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-12 10:04:28 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770890457292' => 'Halló frá grunnþýðingarprófi',
-  'e2e_modify_tr_1770890476031' => 'Breytt gildi fyrir próf á breytingu',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770890457292' => 'Hallo vanuit de basistest',
-  'e2e_context_tr_1770890542100' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_modify_tr_1770890476031' => 'Gewijzigde waarde voor test van de wijziging',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
