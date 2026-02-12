1913ed264dcbc5e1f74353126d61e2e577a0ba3a
LocaleFlow <noreply@localeflow.io>
2026-02-12 13:21:13 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770902391697' => 'Hello from basic translation test',
-  'e2e_delete_tr_1770902448818' => 'Value to delete',
   'e2e_modify_tr_1770902410449' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770902391697' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_delete_tr_1770902448818' => 'Eyða gildi',
   'e2e_modify_tr_1770902410449' => 'Breytt gildi fyrir próf á breytingu',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
