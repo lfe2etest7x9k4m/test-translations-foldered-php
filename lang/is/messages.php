57f5430ad80f44edfb6b452768bc23ab44a297d9
LocaleFlow <noreply@localeflow.io>
2026-02-12 13:20:40 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770902391697' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_modify_tr_1770902410449' => 'Upphaflegt gildi fyrir breytingarprófun',
+  'e2e_modify_tr_1770902410449' => 'Breytt gildi fyrir próf á breytingu',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
