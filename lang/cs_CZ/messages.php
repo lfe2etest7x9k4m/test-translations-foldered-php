2d8d3eeb14b587dd3b2db93059717fa1887195b4
LocaleFlow <noreply@localeflow.io>
2026-02-12 14:39:06 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770907115778' => 'Ahoj z testu základního překladu',
+  'e2e_modify_tr_1770907134537' => 'Počáteční hodnota pro test úpravy',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770907115778' => 'Hello from a basic translation test',
+  'e2e_modify_tr_1770907134537' => 'Initial value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
