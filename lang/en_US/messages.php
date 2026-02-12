e5440b55f467905dc550006382f174b63ea493af
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-12 14:05:52 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770904924819' => 'Hello from basic translation test',
-  'e2e_context_tr_1770905009588' => 'Please use formal language for this text',
-  'e2e_modify_tr_1770904943514' => 'Modified value for modify test',
-  'e2e_ws_rt_1770905072272' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770904924819' => 'Hallo van de basistestvertaling',
-  'e2e_context_tr_1770905009588' => 'Please use formal language for this text',
-  'e2e_modify_tr_1770904943514' => 'Gewijzigde waarde voor testwijziging',
-  'e2e_ws_rt_1770905072272' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
