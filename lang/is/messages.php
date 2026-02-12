c644b9f0c0f883ff55b4bace5fe6ad605cb54993
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-12 13:18:09 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770901989113' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_context_tr_1770902073532' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
-  'e2e_modify_tr_1770902007835' => 'Breytt gildi fyrir próf á breytingu',
-  'e2e_ws_rt_1770902135225' => 'Prófunargildi fyrir WebSocket í rauntíma',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
