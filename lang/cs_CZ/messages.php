27beb1e3023146f93512b255c8804ecdbc2167d8
LocaleFlow <noreply@localeflow.io>
2026-02-12 14:46:25 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770907115778' => 'Ahoj z testu základního překladu',
   'e2e_context_tr_1770907200084' => 'Prosím, použijte pro tento text formální jazyk',
   'e2e_modify_tr_1770907134537' => 'Upravená hodnota pro test úpravy',
+  'e2e_pr_tr_1770907572697' => 'Testovací hodnota PR',
   'e2e_rt21_ws_1770907461065' => 'Test aktualizace využití RT-21',
   'e2e_rt24_ws_1770907323254' => 'RT-24 test automatického doplnění',
   'e2e_ws_rt_1770907262535' => 'Hodnota testu WebSocket v reálném čase',
