642411129b7e34b00a0c716ec8b5c27a0c942eb9
LocaleFlow <noreply@localeflow.io>
2026-02-12 20:57:29 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770929773385' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_delete_tr_1770929825246' => 'Gildi til að eyða',
   'e2e_modify_tr_1770929792204' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
