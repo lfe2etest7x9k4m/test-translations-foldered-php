89104cc2ec46a7771fd5acbb26348724e3b2e8c7
LocaleFlow <noreply@localeflow.io>
2026-02-12 11:24:03 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770895375839' => 'Zdravím z testu základního překladu',
+  'e2e_delete_tr_1770895427289' => 'Hodnota ke smazání',
   'e2e_modify_tr_1770895394411' => 'Upravená hodnota pro test úpravy',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
