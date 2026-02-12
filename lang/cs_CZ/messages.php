01736a528833ee34066ea71ba192108819907e26
LocaleFlow <noreply@localeflow.io>
2026-02-12 14:49:22 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770907115778' => 'Ahoj z testu základního překladu',
   'e2e_context_tr_1770907200084' => 'Prosím, použijte pro tento text formální jazyk',
+  'e2e_err10_1770907730737' => 'Test odmítnutí push',
   'e2e_modify_tr_1770907134537' => 'Upravená hodnota pro test úpravy',
   'e2e_multi_lang_1770907657818' => 'Manuální úprava v britské angličtině',
   'e2e_pr_tr_1770907572697' => 'Testovací hodnota PR',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770907115778' => 'Hallo van de eenvoudige vertalingstest',
   'e2e_context_tr_1770907200084' => 'Gebruik alstublieft formele taal voor deze tekst.',
+  'e2e_err10_1770907730737' => 'Test voor afwijzing van push',
   'e2e_modify_tr_1770907134537' => 'Gewijzigde waarde voor wijzigingstest',
   'e2e_multi_lang_1770907657818' => 'Handmatige bewerking in Brits-Engels',
   'e2e_pr_tr_1770907572697' => 'Testwaarde PR',
