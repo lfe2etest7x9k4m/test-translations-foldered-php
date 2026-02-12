edfb2d199e18908b80782c5bbd97ade80eea80b4
LocaleFlow <noreply@localeflow.io>
2026-02-12 10:29:29 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770892158637' => 'Ahoj z testu základního překladu',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/es_US/messages.php
+++ lang/es_US/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770892158637' => 'Hola desde la prueba de traducción básica',
   'goodbye' => 'Adiós',
   'greeting' => 'Hola, :name!',
   'welcome' => 'Bienvenido a nuestra aplicación',
