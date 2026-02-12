9c18b157ca1d7e072a21dfbcc8866bd550719616
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-12 22:27:59 +0000

    Test EDGE-01: Delete key in en_GB, modify in en_US

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -13,6 +13,4 @@ return [
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
-  'welcome' => 'Welcome to our application',
-    'e2e_update_precedence_key' => 'en_GB original',
-];
+  'welcome' => 'Welcome to our application',];
