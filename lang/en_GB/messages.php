480406dc682a02a94d4555f8f0c4c334eaaf9aa1
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-12 22:30:05 +0000

    E2E test: Add e2e_bp03_1770935403218 for BP-03 GitHub protection detection

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -17,4 +17,5 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
+    'e2e_bp03_1770935403218' => 'GitHub protection test',
 ];
