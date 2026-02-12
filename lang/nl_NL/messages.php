979f6e00fdaff7deeae79b2bac907fe4af0c3887
LocaleFlow <noreply@localeflow.io>
2026-02-12 23:00:36 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770936575390' => 'Hallo vanuit de basisvertalingstest',
+  'e2e_bp03_1770937219434' => 'GitHub-beschermingstest',
   'e2e_context_tr_1770936659285' => 'Gelieve formele taal te gebruiken voor deze tekst',
   'e2e_err10_1770937181081' => 'Test van pushafwijzing',
   'e2e_modify_tr_1770936594000' => 'Gewijzigde waarde voor test voor wijziging',
