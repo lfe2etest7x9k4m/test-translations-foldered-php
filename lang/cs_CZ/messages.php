c03c8eefc3de66c0ddf6f216f9d4325a2f56557d
LocaleFlow <noreply@localeflow.io>
2026-02-12 10:02:14 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770890457292' => 'Zdravím ze základního testu překladu',
-  'e2e_delete_tr_1770890509257' => 'Hodnota k odstranění',
   'e2e_modify_tr_1770890476031' => 'Upravená hodnota pro test úprav',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770890457292' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1770890509257' => 'Gildi til að eyða',
   'e2e_modify_tr_1770890476031' => 'Breytt gildi fyrir próf á breytingu',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
