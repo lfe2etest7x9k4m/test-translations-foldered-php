c96921f9b4b6fde99e27d3580500dad2f90aee25
LocaleFlow <noreply@localeflow.io>
2026-02-12 21:25:37 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770930881443' => 'Hallo aus dem einfachen Übersetzungstest',
+  'e2e_bp03_1770931519136' => 'Test für GitHub-Schutz',
   'e2e_context_tr_1770930965199' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_err10_1770931491385' => 'Test der Push-Zurückweisung',
   'e2e_modify_tr_1770930900075' => 'Geänderter Wert für den Modifikationstest',
