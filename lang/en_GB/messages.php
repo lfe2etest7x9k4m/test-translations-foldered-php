a37af1afe4385fb8eab66c322c9edde1b447da4d
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-13 04:02:36 +0000

    Test QUOTA-05: e2e_quota_pause_1770955355629

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -11,4 +11,5 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
+    'e2e_quota_pause_1770955355629' => 'Quota pause test',
 ];
