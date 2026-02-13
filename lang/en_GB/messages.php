597a29770b36ec29592a8e18bdf290a9c289b0bb
LocaleFlow <noreply@localeflow.io>
2026-02-13 05:18:02 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,10 +4,10 @@ return [
   'e2e_basic_tr_1770959539364' => 'Hello from basic translation test',
   'e2e_context_tr_1770959623215' => 'Please use formal language for this text',
   'e2e_modify_tr_1770959558021' => 'Modified value for modify test',
+  'e2e_rt21_ws_1770959872156' => 'RT-21 usage update test',
   'e2e_rt24_ws_1770959737810' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770959679657' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
-    'e2e_rt21_ws_1770959872156' => 'RT-21 usage update test',
 ];
