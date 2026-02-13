28e668298de77c77712fb198b5ae57ee8b036837
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-13 00:01:03 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,19 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770939807526' => 'Hello from basic translation test',
-  'e2e_context_tr_1770939891558' => 'Please use formal language for this text',
-  'e2e_modify_tr_1770939826183' => 'Modified value for modify test',
-  'e2e_multi_lang_1770940348566' => 'American English manual edit',
-  'e2e_pr_tr_1770940265199' => 'PR test value',
-  'e2e_quota_pause_1770940209853' => 'Quota pause test',
-  'e2e_rt05_ws_1770940221664' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770940150278' => 'RT-21 usage update test',
-  'e2e_rt24_ws_1770940012998' => 'RT-24 auto top-up test',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'e2e_ws_rt_1770939953653' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
