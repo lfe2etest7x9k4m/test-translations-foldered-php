5fb52651aed02d4cb61367f04a45d9fcd54d89a4
LocaleFlow <noreply@localeflow.io>
2026-02-13 04:28:48 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770956852549' => 'Bonjour depuis le test de traduction basique',
-  'e2e_delete_tr_1770956904264' => 'Valeur à supprimer',
   'e2e_modify_tr_1770956871409' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770956852549' => 'Halló frá einfaldri þýðingarprófi',
-  'e2e_delete_tr_1770956904264' => 'Gildi til að eyða',
   'e2e_modify_tr_1770956871409' => 'Breytt gildi fyrir breytingarprófun',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
