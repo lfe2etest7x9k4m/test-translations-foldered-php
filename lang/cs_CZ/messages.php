6b1d638c1c0b3c632ac2f850cc8ce0d3ce202896
LocaleFlow <noreply@localeflow.io>
2026-02-13 03:59:42 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770954970400' => 'Ahoj z testu základního překladu',
   'e2e_context_tr_1770955053828' => 'Prosím, použijte pro tento text formální jazyk',
   'e2e_modify_tr_1770954989051' => 'Upravená hodnota pro test úpravy',
+  'e2e_rt24_ws_1770955171809' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770955116077' => 'Hodnota testu WebSocket v reálném čase',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
