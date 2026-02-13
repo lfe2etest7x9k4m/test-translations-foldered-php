5f64eeb63ac95f7e26bfba41609cfb6401330c75
LocaleFlow <noreply@localeflow.io>
2026-02-13 04:03:47 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770954970400' => 'Halló frá grunnprófun í þýðingu',
   'e2e_context_tr_1770955053828' => 'Vinsamlegast notið formlegt orðalag í þessum texta',
   'e2e_modify_tr_1770954989051' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_pr_tr_1770955411477' => 'PR prófun',
   'e2e_rt21_ws_1770955306084' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_rt24_ws_1770955171809' => 'RT-24 sjálfvirk áfyllingarprófun',
   'e2e_ws_rt_1770955116077' => 'Prófunargildi fyrir rauntíma WebSocket',
