f71527b73cf4589b7645219e649c15bda387a88b
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-13 01:36:34 +0000

    E2E test: Add e2e_pr_tr_1770946591793 for PR test

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -13,4 +13,5 @@ return [
   'welcome' => 'Welcome to our application',
     'e2e_quota_pause_1770946535985' => 'Quota pause test',
     'e2e_rt05_ws_1770946547868' => 'RT-05 WebSocket test',
+    'e2e_pr_tr_1770946591793' => 'PR test value',
 ];
