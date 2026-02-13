699e297a4edfa1152696c9c88a55ffefe66129d2
LocaleFlow <noreply@localeflow.io>
2026-02-13 07:00:55 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770965986928' => 'Halló frá einföldu þýðingarprófi',
+  'e2e_delete_tr_1770966038799' => 'Gildi til að eyða',
   'e2e_modify_tr_1770966005713' => 'Breytt gildi fyrir breytingarprófun',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770965986928' => 'Hallo vanuit de basisvertalingstest',
+  'e2e_delete_tr_1770966038799' => 'Te verwijderen waarde',
   'e2e_modify_tr_1770966005713' => 'Gewijzigde waarde voor test voor wijziging',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
