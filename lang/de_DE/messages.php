b38264c90df0989c39e35f4865558f74e739291f
LocaleFlow <noreply@localeflow.io>
2026-02-13 03:42:56 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770954106128' => 'Hallo aus dem grundlegenden Übersetzungstest',
+  'e2e_delete_tr_1770954158114' => 'Zu löschender Wert',
   'e2e_modify_tr_1770954124704' => 'Geänderter Wert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
