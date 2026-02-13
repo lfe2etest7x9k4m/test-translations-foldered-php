6397b33154cf638969138267c3d7a2a807a22622
LocaleFlow <noreply@localeflow.io>
2026-02-13 01:16:23 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1770944868040' => 'Hello from basic translation test',
   'e2e_context_tr_1770944951683' => 'Please use formal language for this text',
   'e2e_modify_tr_1770944886717' => 'Modified value for modify test',
-  'e2e_pr_tr_1770945357366' => 'PR test value',
+  'e2e_pr_tr_1770945357366' => 'Test value for pull request',
   'e2e_quota_pause_1770945258828' => 'Quota pause test',
   'e2e_rt05_ws_1770945270610' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770945206409' => 'RT-21 usage update test',
