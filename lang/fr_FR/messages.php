cccaab56fa8450dac6f9ed5cb2d3354eb603cab4
LocaleFlow <noreply@localeflow.io>
2026-02-13 03:11:04 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770951914687' => 'Bonjour, test de traduction de base',
   'e2e_context_tr_1770951998563' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_modify_tr_1770951933314' => 'Valeur modifiée pour le test de modification',
+  'e2e_rt21_ws_1770952254151' => 'Test de mise à jour de l\'utilisation RT-21',
   'e2e_rt24_ws_1770952119152' => 'Test de recharge automatique RT-24',
   'e2e_ws_rt_1770952060545' => 'Valeur de test WebSocket en temps réel',
   'goodbye' => 'Au revoir',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770951914687' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1770951998563' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
   'e2e_modify_tr_1770951933314' => 'Breytt gildi fyrir breytingarprófun',
+  'e2e_rt21_ws_1770952254151' => 'RT-21 prófun á notkunaruppfærslu',
   'e2e_rt24_ws_1770952119152' => 'RT-24 prófun á sjálfvirkri áfyllingu',
   'e2e_ws_rt_1770952060545' => 'Prófunargildi fyrir WebSocket í rauntíma',
   'goodbye' => 'Bless',
