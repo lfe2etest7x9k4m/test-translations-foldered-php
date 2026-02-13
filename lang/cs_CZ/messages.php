818bdefef3f4917322afed3e846eb7091a3607f4
LocaleFlow <noreply@localeflow.io>
2026-02-13 07:00:31 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770965986928' => 'Ahoj z testu základního překladu',
-  'e2e_modify_tr_1770966005713' => 'Počáteční hodnota pro test úprav',
+  'e2e_modify_tr_1770966005713' => 'Upravená hodnota pro test úpravy',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770965986928' => 'Hallo von einem einfachen Übersetzungstest',
-  'e2e_modify_tr_1770966005713' => 'Anfangswert für Modifikationstest',
+  'e2e_modify_tr_1770966005713' => 'Geänderter Wert für Modifikationstest',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
