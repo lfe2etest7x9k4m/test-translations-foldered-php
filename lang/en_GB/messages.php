ba2d4f9ea2039168185ef5890317e900c28f01b9
LocaleFlow <noreply@localeflow.io>
2026-02-13 04:22:17 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1770955900301' => 'Hello from basic translation test',
-  'e2e_bp03_1770956495189' => 'GitHub protection test',
+  'e2e_bp03_1770956495189' => 'Testing GitHub protection',
   'e2e_context_tr_1770955985005' => 'Please use formal language for this text',
   'e2e_err10_1770956471774' => 'Push rejection test',
   'e2e_modify_tr_1770955919448' => 'Modified value for modify test',
