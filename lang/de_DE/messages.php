ed83fd0a2d7e6ea77189c12dc635b7dd72195e16
LocaleFlow <noreply@localeflow.io>
2026-02-13 01:11:21 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770944868040' => 'Hallo aus dem grundlegenden Übersetzungstest',
   'e2e_context_tr_1770944951683' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1770944886717' => 'Geänderter Wert für Änderungstest',
+  'e2e_rt24_ws_1770945071878' => 'RT-24 automatischer Aufladungstest',
   'e2e_ws_rt_1770945013159' => 'WebSocket Echtzeit-Testwert',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770944868040' => 'Hello from basic translation test',
   'e2e_context_tr_1770944951683' => 'Please use formal language for this text',
   'e2e_modify_tr_1770944886717' => 'Modified value for modify test',
+  'e2e_rt24_ws_1770945071878' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1770945013159' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
