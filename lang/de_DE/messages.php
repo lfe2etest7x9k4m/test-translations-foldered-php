f38afd34e9e501d9c735706ae9739d39a0b94af9
LocaleFlow <noreply@localeflow.io>
2026-02-13 02:27:01 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770949120467' => 'Hallo aus dem grundlegenden Übersetzungstest',
   'e2e_context_tr_1770949204537' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1770949139638' => 'Geänderter Wert für Modifikationstest',
+  'e2e_pr_tr_1770949609126' => 'PR-Testwert',
   'e2e_rt21_ws_1770949457515' => 'RT-21 Nutzungsaktualisierungstest',
   'e2e_rt24_ws_1770949322738' => 'RT-24 Test für automatische Aufladung',
   'e2e_ws_rt_1770949266177' => 'WebSocket Echtzeit-Testwert',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770949120467' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1770949204537' => 'Vinsamlegast notið formlega orðræðu fyrir þennan texta',
   'e2e_modify_tr_1770949139638' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_pr_tr_1770949609126' => 'Prófunargildi fyrir PR',
   'e2e_rt21_ws_1770949457515' => 'RT-21 notkunaruppfærslingarpróf',
   'e2e_rt24_ws_1770949322738' => 'RT-24 prófun á sjálfvirkri áfyllingu',
   'e2e_ws_rt_1770949266177' => 'Rauntíma prófunargildi fyrir WebSocket',
