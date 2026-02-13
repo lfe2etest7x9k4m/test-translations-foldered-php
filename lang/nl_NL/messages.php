b91b75c4d7cd672c173e48ec6e1c4b1975967f2f
LocaleFlow <noreply@localeflow.io>
2026-02-13 02:29:02 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770949120467' => 'Hallo vanuit de basisvertalingstest',
   'e2e_context_tr_1770949204537' => 'Gebruik alstublieft formele taal voor deze tekst',
+  'e2e_err10_1770949731626' => 'Test voor pushafwijzing',
   'e2e_modify_tr_1770949139638' => 'Gewijzigde waarde voor de wijzigingstest',
   'e2e_multi_lang_1770949691929' => 'Handmatige bewerking in Brits-Engels',
   'e2e_pr_tr_1770949609126' => 'PR-testwaarde',
