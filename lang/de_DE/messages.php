9047e2052216887a18daadd21bbe65e35ccfb39d
LocaleFlow <noreply@localeflow.io>
2026-02-13 02:18:53 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770949120467' => 'Hallo aus dem grundlegenden Übersetzungstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen in unserer App',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770949120467' => 'Bonjour depuis le test de traduction basique',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
