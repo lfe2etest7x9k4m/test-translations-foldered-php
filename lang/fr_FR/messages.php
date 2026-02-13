2fae89b4fd75124d5b4a0044f6de64ddc15ce27a
LocaleFlow <noreply@localeflow.io>
2026-02-13 06:18:54 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770962752816' => 'Bonjour depuis l\'essai de traduction basique',
+  'e2e_bp03_1770963519258' => 'Test de protection sur GitHub',
   'e2e_context_tr_1770962892277' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_err10_1770963496582' => 'Test de rejet du push',
   'e2e_modify_tr_1770962771520' => 'Valeur modifiée pour le test de modifier',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770962752816' => 'Halló frá einföldu þýðingarprófi',
+  'e2e_bp03_1770963519258' => 'Prófun á verndun GitHub',
   'e2e_context_tr_1770962892277' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
   'e2e_err10_1770963496582' => 'Prófun á höfnun kóðafærslu',
   'e2e_modify_tr_1770962771520' => 'Breytt prófun',
