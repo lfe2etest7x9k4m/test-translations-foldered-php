27eab55f0d4bca9e24b46462da3ed10e1d23ce0d
LocaleFlow <noreply@localeflow.io>
2026-02-13 02:53:30 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770950601446' => 'Zdravím z testu základního překladu',
   'e2e_context_tr_1770950684959' => 'Prosím, použijte pro tento text formální jazyk.',
+  'e2e_err10_1770951199338' => 'Test zamítnutí push',
   'e2e_modify_tr_1770950620107' => 'Upravená hodnota pro test úpravy',
   'e2e_multi_lang_1770951130237' => 'Manuální úprava britské angličtiny',
   'e2e_pr_tr_1770951042554' => 'Testovací hodnota PR',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1770950601446' => 'Hallo vanuit de basisvertalingstest',
   'e2e_context_tr_1770950684959' => 'Please use formal language for this text',
+  'e2e_err10_1770951199338' => 'Test op afwijzing van push',
   'e2e_modify_tr_1770950620107' => 'Gewijzigde waarde voor test van wijziging',
   'e2e_multi_lang_1770951130237' => 'Brits-Engelse handmatige bewerking',
   'e2e_pr_tr_1770951042554' => 'PR-testwaarde',
