0191d0e0066fd96d36c1c12c06a220833c06874c
LocaleFlow <noreply@localeflow.io>
2026-02-13 06:07:56 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770962752816' => 'Ahoj z testu základního překladu',
+  'e2e_delete_tr_1770962860275' => 'Hodnota k odstranění',
   'e2e_modify_tr_1770962771520' => 'Upravená hodnota pro testování úprav',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770962752816' => 'Hallo vanuit de basisvertalingstest',
+  'e2e_delete_tr_1770962860275' => 'Te verwijderen waarde',
   'e2e_modify_tr_1770962771520' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
