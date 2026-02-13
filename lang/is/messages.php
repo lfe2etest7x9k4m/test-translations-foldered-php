9fcfb37abc22fad38b6542756f86c5c630233e7e
LocaleFlow <noreply@localeflow.io>
2026-02-13 07:26:06 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -9,5 +9,6 @@ return [
   'e2e_ws_rt_1770967323431' => 'Gildi fyrir rauntíma WebSocket prófun',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'quota_test_key' => 'Prófunargildi',
   'welcome' => 'Velkomin í forritið okkar',
 ];
