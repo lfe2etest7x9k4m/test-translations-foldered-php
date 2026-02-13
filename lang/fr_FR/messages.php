c27dc7816c79bf8f429531c211ba13c3e4f32177
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-13 02:27:23 +0000

    Setup: Add/update e2e_update_precedence_key in all languages

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -12,4 +12,5 @@ return [
   'greeting' => 'Halló, :name!',
   'quota_test_key' => 'Valeur test',
   'welcome' => 'Velkomin í forritið okkar',
+    'e2e_update_precedence_key' => 'fr_FR original',
 ];
