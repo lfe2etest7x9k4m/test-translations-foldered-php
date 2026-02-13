dcc6c00fa3370119e6607e17588d4898eb6f7d4f
LocaleFlow <noreply@localeflow.io>
2026-02-13 05:13:36 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770959539364' => 'Zdravím z testu základního překladu',
-  'e2e_delete_tr_1770959591069' => 'Hodnota k odstranění',
   'e2e_modify_tr_1770959558021' => 'Upravená hodnota pro test úprav',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1770959539364' => 'Hello from basic translation test',
-  'e2e_delete_tr_1770959591069' => 'Value to delete',
   'e2e_modify_tr_1770959558021' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
