a53c12f62515a98dbc143916ab8e8800513f640d
LocaleFlow <noreply@localeflow.io>
2026-02-13 05:29:24 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770960553017' => 'Zdravím z testování základního překladu',
   'goodbye' => 'Nashledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1770960553017' => 'Halló frá grunnprófunarprófi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
