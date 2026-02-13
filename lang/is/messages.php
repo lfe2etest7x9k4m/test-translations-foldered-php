c07cfa8b15941c3ecc4bcb816348ede5d387d847
LocaleFlow <noreply@localeflow.io>
2026-02-13 07:07:54 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -7,6 +7,7 @@ return [
   'e2e_pr_tr_1770966435435' => 'PR prófun',
   'e2e_rt21_ws_1770966323051' => 'Prófun á uppfærslu á RT-21 notkun',
   'e2e_rt24_ws_1770966188867' => 'RT-24 sjálfvirk áfyllingartilraun',
+  'e2e_update_precedence_key' => 'is original',
   'e2e_ws_rt_1770966133450' => 'Rauntíma prófunargildi fyrir WebSocket',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -7,6 +7,7 @@ return [
   'e2e_pr_tr_1770966435435' => 'PR testwaarde',
   'e2e_rt21_ws_1770966323051' => 'RT-21 gebruiksupdatetest',
   'e2e_rt24_ws_1770966188867' => 'RT-24 automatische aanvulling test',
+  'e2e_update_precedence_key' => 'nl_NL original',
   'e2e_ws_rt_1770966133450' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
