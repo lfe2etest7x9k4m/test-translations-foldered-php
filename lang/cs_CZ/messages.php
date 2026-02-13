613a40abdadf733945427a3d62061e4edcd92a1a
LocaleFlow <noreply@localeflow.io>
2026-02-13 00:49:17 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770943259897' => 'Zdravím z testu základního překladu',
   'e2e_context_tr_1770943343685' => 'Prosím, použijte pro tento text formální jazyk',
   'e2e_modify_tr_1770943278611' => 'Upravená hodnota pro testování úpravy',
+  'e2e_pr_tr_1770943745369' => 'Testovací hodnota PR',
   'e2e_rt21_ws_1770943597026' => 'Test aktualizace používání RT-21',
   'e2e_rt24_ws_1770943462712' => 'Test automatického doplnění RT-24',
   'e2e_ws_rt_1770943405263' => 'Testovací hodnota WebSocket v reálném čase',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770943259897' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1770943343685' => 'Vinsamlegast notið formlegt mál notkun fyrir þennan texta.',
   'e2e_modify_tr_1770943278611' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_pr_tr_1770943745369' => 'Prófunargildi fyrir PR',
   'e2e_rt21_ws_1770943597026' => 'RT-21 prófun á uppfærslu notkunar',
   'e2e_rt24_ws_1770943462712' => 'RT-24 prófun á sjálfvirkri áfyllingu',
   'e2e_ws_rt_1770943405263' => 'Gildi fyrir rauntíma prófun á WebSocket',
