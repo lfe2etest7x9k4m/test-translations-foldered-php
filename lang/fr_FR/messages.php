3ffddbc836c0170fc37738d8dfeffb64db60acc6
LocaleFlow <noreply@localeflow.io>
2026-02-13 00:44:32 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770943259897' => 'Bonjour depuis le test de traduction basique',
   'e2e_context_tr_1770943343685' => 'Veuillez utiliser un langage soutenu pour ce texte',
   'e2e_modify_tr_1770943278611' => 'Valeur modifiée pour le test de modification',
+  'e2e_rt24_ws_1770943462712' => 'Test de recharge automatique RT-24',
   'e2e_ws_rt_1770943405263' => 'Valeur de test en temps réel WebSocket',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770943259897' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1770943343685' => 'Vinsamlegast notið formlegt mál notkun fyrir þennan texta.',
   'e2e_modify_tr_1770943278611' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_rt24_ws_1770943462712' => 'RT-24 prófun á sjálfvirkri áfyllingu',
   'e2e_ws_rt_1770943405263' => 'Gildi fyrir rauntíma prófun á WebSocket',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
