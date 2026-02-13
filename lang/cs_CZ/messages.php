a4c1e8fe7369269b07079831bbbc9cf35797dcc7
LocaleFlow <noreply@localeflow.io>
2026-02-13 07:50:18 +0000

    Automatic translations

--- lang/cs_CZ/messages.php
+++ lang/cs_CZ/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770968855128' => 'Ahoj ze základního testu překladu',
   'e2e_context_tr_1770968945091' => 'Prosím, použijte pro tento text formální jazyk',
   'e2e_modify_tr_1770968873856' => 'Upravená hodnota pro testování úpravy',
+  'e2e_ws_rt_1770969006484' => 'Testovací hodnota WebSocket v reálném čase',
   'goodbye' => 'Na shledanou',
   'greeting' => 'Ahoj, :name!',
   'welcome' => 'Vítejte v naší aplikaci',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1770968855128' => 'Hallo vanuit de basis vertaaltest',
   'e2e_context_tr_1770968945091' => 'Gelieve formele taal te gebruiken voor deze tekst',
   'e2e_modify_tr_1770968873856' => 'Gewijzigde waarde voor test van de wijziging',
+  'e2e_ws_rt_1770969006484' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
