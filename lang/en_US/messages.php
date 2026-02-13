3bc06af38627fe00e5f417dfa594433110320e27
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-13 04:26:05 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,21 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770955900301' => 'Hello from basic translation test',
-  'e2e_bp03_1770956495189' => 'GitHub protection test',
-  'e2e_context_tr_1770955985005' => 'Please use formal language for this text',
-  'e2e_err10_1770956471774' => 'Push rejection test',
-  'e2e_modify_tr_1770955919448' => 'Modified value for modify test',
-  'e2e_multi_lang_1770956423689' => 'American English manual edit',
-  'e2e_pr_tr_1770956345284' => 'PR test value',
-  'e2e_quota_pause_1770956289799' => 'Quota pause test',
-  'e2e_rt05_ws_1770956301789' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770956239885' => 'RT-21 usage update test',
-  'e2e_rt24_ws_1770956105060' => 'RT-24 auto top-up test',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'e2e_ws_rt_1770956046777' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,21 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770955900301' => 'Halló frá grunnprófun á þýðingu',
-  'e2e_bp03_1770956495189' => 'Prófun á verndun GitHub',
-  'e2e_context_tr_1770955985005' => 'Vinsamlegast notið formlegt málnotkun fyrir þennan texta',
-  'e2e_err10_1770956471774' => 'Prófun á höfnun við ýtingu',
-  'e2e_modify_tr_1770955919448' => 'Breytt gildi fyrir prófun á að breyta',
-  'e2e_multi_lang_1770956423689' => 'Handvirk breyting (bresk enska)',
-  'e2e_pr_tr_1770956345284' => 'PR prófunargildi',
-  'e2e_quota_pause_1770956289799' => 'Prófun á kvótahléi',
-  'e2e_rt05_ws_1770956301789' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770956239885' => 'RT-21 próf á notkunaruppfærslu',
-  'e2e_rt24_ws_1770956105060' => 'RT-24 prófun á sjálfvirkri áfyllingu',
-  'e2e_update_precedence_key' => 'is original',
-  'e2e_ws_rt_1770956046777' => 'Prófunargildi fyrir WebSocket í rauntíma',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
