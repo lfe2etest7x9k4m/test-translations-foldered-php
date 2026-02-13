21d51565c4648af0739d8881d505aa3663de65c2
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-13 01:27:45 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,19 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770944868040' => 'Halló frá grunnþýðingarprófunar',
-  'e2e_context_tr_1770944951683' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
-  'e2e_modify_tr_1770944886717' => 'Breytt gildi fyrir breytingarprófun',
-  'e2e_pr_tr_1770945357366' => 'Prófunargildi fyrir pull request',
-  'e2e_quota_pause_1770945258828' => 'Prófun á kvóta hlés',
-  'e2e_rt05_ws_1770945270610' => 'Prófun á RT-05 WebSocket-s',
-  'e2e_rt21_ws_1770945206409' => 'RT-21 notkunaruppfærslingarpróf',
-  'e2e_rt24_ws_1770945071878' => 'RT-24 prófun á sjálfvirkri áfyllingu',
-  'e2e_update_precedence_key' => 'is original',
-  'e2e_ws_rt_1770945013159' => 'WebSocket rauntíma prófunarverðmæti',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_multi_lang_1770945445425' => 'is original',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,19 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1770944868040' => 'Hallo vanuit de basisvertalingstest',
-  'e2e_context_tr_1770944951683' => 'Gebruik alstublieft formele taal voor deze tekst',
-  'e2e_modify_tr_1770944886717' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_pr_tr_1770945357366' => 'PR-testwaarde',
-  'e2e_quota_pause_1770945258828' => 'Quotapauze test',
-  'e2e_rt05_ws_1770945270610' => 'RT-05 WebSocket test',
-  'e2e_rt21_ws_1770945206409' => 'RT-21 gebruiksupdate test',
-  'e2e_rt24_ws_1770945071878' => 'RT-24 automatische bijvulling test',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'e2e_ws_rt_1770945013159' => 'WebSocket realtime testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
-    'e2e_multi_lang_1770945445425' => 'nl_NL original',
 ];
