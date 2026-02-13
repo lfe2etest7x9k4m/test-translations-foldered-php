c2c6486fd410b5cad2bbb44c877fa5242ea5b885
LocaleFlow <noreply@localeflow.io>
2026-02-13 06:16:26 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -9,6 +9,7 @@ return [
   'e2e_rt05_ws_1770963271213' => 'RT-05 WebSocket-Test',
   'e2e_rt21_ws_1770963202829' => 'Test der RT-21 Nutzungsaktualisierung',
   'e2e_rt24_ws_1770963068204' => 'RT-24 automatische Aufladungstest',
+  'e2e_update_precedence_key' => 'de_DE original',
   'e2e_ws_rt_1770963009626' => 'WebSocket Echtzeit-Testwert',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -9,6 +9,7 @@ return [
   'e2e_rt05_ws_1770963271213' => 'RT-05 WebSocket test',
   'e2e_rt21_ws_1770963202829' => 'RT-21 test van gebruiksupdate',
   'e2e_rt24_ws_1770963068204' => 'RT-24 auto bijvulling test',
+  'e2e_update_precedence_key' => 'nl_NL original',
   'e2e_ws_rt_1770963009626' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
