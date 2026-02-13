eefaeea8497bf8a8c4b13af2730054f5249325b1
LocaleFlow <noreply@localeflow.io>
2026-02-13 03:16:05 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770951914687' => 'Hallo von grundlegenden Übersetzungstests',
+  'e2e_bp03_1770952549909' => 'GitHub-Schutz-Test',
   'e2e_context_tr_1770951998563' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_err10_1770952527411' => 'Push-Ablehnungs-Test',
   'e2e_modify_tr_1770951933314' => 'Geänderter Wert für Modifikationstest',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1770951914687' => 'Bonjour, test de traduction de base',
+  'e2e_bp03_1770952549909' => 'Test de protection sur GitHub',
   'e2e_context_tr_1770951998563' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_err10_1770952527411' => 'Test de rejet de push',
   'e2e_modify_tr_1770951933314' => 'Valeur modifiée pour le test de modification',
