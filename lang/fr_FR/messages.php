5d6382814996a6847bc83f636e333e81c8ba38cd
LocaleFlow <noreply@localeflow.io>
2026-02-18 20:34:27 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1771446636193' => 'Bonjour depuis le test de traduction basique',
-  'e2e_modify_tr_1771446744392' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1771446744392' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1771446636193' => 'Hallo vanuit de basistest voor vertalingen',
-  'e2e_modify_tr_1771446744392' => 'Initiële waarde voor wijzigingstest',
+  'e2e_modify_tr_1771446744392' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
