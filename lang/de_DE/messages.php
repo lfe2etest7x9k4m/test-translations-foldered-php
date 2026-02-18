a67e22318452812c2f6f6008b44192a72baffe89
LocaleFlow <noreply@localeflow.io>
2026-02-18 18:43:40 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771439256661' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_context_tr_1771439421979' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1771439297134' => 'Geänderter Wert für den Änderungs-Test',
-  'e2e_multi_lang_1771440146733' => 'de_DE original',
+  'e2e_multi_lang_1771440146733' => 'Manuelle Bearbeitung (brit. Englisch)',
   'e2e_pr_tr_1771440007139' => 'PR-Testwert',
   'e2e_rt21_ws_1771439812888' => 'RT-21 Nutzungsaktualisierungstest',
   'e2e_rt24_ws_1771439675289' => 'RT-24 Test der automatischen Aufladung',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771439256661' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1771439421979' => 'Vinsamlegast notið formála tungutak fyrir þennan texta',
   'e2e_modify_tr_1771439297134' => 'Breytt gildi fyrir prófun á breytingu',
-  'e2e_multi_lang_1771440146733' => 'is original',
+  'e2e_multi_lang_1771440146733' => 'Breskar enskar handvirk breyting',
   'e2e_pr_tr_1771440007139' => 'PR prófun',
   'e2e_rt21_ws_1771439812888' => 'RT-21 prófun á notkunaruppfærslu',
   'e2e_rt24_ws_1771439675289' => 'RT-24 prófun á sjálfvirkri áfyllingu',
