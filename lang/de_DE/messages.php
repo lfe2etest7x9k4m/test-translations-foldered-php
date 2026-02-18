dcdb9740ba152b161b6202a3a0c6b52b9bf07e20
LocaleFlow <noreply@localeflow.io>
2026-02-18 20:40:17 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771446636193' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_context_tr_1771446931399' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache.',
   'e2e_modify_tr_1771446744392' => 'Geänderter Wert für den Änderungstest',
+  'e2e_ws_rt_1771447174874' => 'WebSocket Echtzeit-Testwert',
   'goodbye' => 'Tschüss',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen in unserer Anwendung',
