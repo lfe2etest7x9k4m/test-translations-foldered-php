26eb6118f4ecfc2b13c428816dc8ed55ffdf3fe1
LocaleFlow <noreply@localeflow.io>
2026-02-18 18:24:18 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1771438603459' => 'Hallo von grundlegenden Übersetzungs Tests',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+  'e2e_basic_tr_1771438603459' => 'Halló frá einföldu þýðingarprófi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
