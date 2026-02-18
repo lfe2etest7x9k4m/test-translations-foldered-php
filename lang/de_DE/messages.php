55bfaa948fdd2246f6517da1ca8b0b158928ac8e
LocaleFlow <noreply@localeflow.io>
2026-02-18 19:25:42 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771441707438' => 'Hallo von grundlegenden Übersetzungstests',
   'e2e_context_tr_1771442026707' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1771441773878' => 'Geänderter Wert für Änderungstest',
+  'e2e_pr_tr_1771442677252' => 'PR Testwert',
   'e2e_rt21_ws_1771442463658' => 'Test der RT-21 Nutzungsaktualisierung',
   'e2e_rt24_ws_1771442326810' => 'RT-24 Auto Top-Up Test',
   'e2e_ws_rt_1771442240465' => 'WebSocket Echtzeit-Testwert',
