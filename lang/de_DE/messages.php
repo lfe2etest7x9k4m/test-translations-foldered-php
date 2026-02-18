cda173a1443cf652c4a1bb838f120a6a34c90a98
LocaleFlow <noreply@localeflow.io>
2026-02-18 20:33:17 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1771446636193' => 'Hallo vom grundlegenden Übersetzungstest',
+  'e2e_modify_tr_1771446744392' => 'Anfangswert für Änderungstest',
   'goodbye' => 'Tschüss',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen in unserer Anwendung',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1771446636193' => 'Halló frá einföldu þýðingarprófi',
+  'e2e_modify_tr_1771446744392' => 'Upphaflegt gildi fyrir prófun á breytingu',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
