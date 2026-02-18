59117602b63630324b2ec78671dd14934499856a
LocaleFlow <noreply@localeflow.io>
2026-02-18 19:22:34 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771441707438' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1771442026707' => 'Please use formal language for this text',
   'e2e_modify_tr_1771441773878' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_rt21_ws_1771442463658' => 'Próf á uppfærslu á notkun RT-21',
   'e2e_rt24_ws_1771442326810' => 'RT-24 prófun á sjálfvirkri áfyllingu',
   'e2e_ws_rt_1771442240465' => 'Prófunargildi fyrir WebSocket í rauntíma',
   'goodbye' => 'Bless',
