2f5420e4c94fdb4837752afabc38379fcd34d6a5
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-18 18:03:51 +0000

    E2E test: Add e2e_basic_tr_1771437829241 to en_GB

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,4 +4,5 @@ return [
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
+    'e2e_basic_tr_1771437829241' => 'Hello from basic translation test',
 ];
