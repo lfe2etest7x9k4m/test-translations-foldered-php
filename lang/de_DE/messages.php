be0f06e2d5a4150be4160aa743a6010b1cffdf5e
LocaleFlow <noreply@localeflow.io>
2026-02-18 18:37:25 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771439256661' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_context_tr_1771439421979' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
   'e2e_modify_tr_1771439297134' => 'Geänderter Wert für den Änderungs-Test',
+  'e2e_rt21_ws_1771439812888' => 'RT-21 Nutzungsaktualisierungstest',
   'e2e_rt24_ws_1771439675289' => 'RT-24 Test der automatischen Aufladung',
   'e2e_ws_rt_1771439577160' => 'WebSocket-Echtzeittestwert',
   'goodbye' => 'Tschüss',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771439256661' => 'Bonjour du test de traduction de base',
   'e2e_context_tr_1771439421979' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_modify_tr_1771439297134' => 'Valeur modifiée pour tester la modification',
+  'e2e_rt21_ws_1771439812888' => 'Test de mise à jour de l\'utilisation RT-21',
   'e2e_rt24_ws_1771439675289' => 'Test de recharge automatique RT-24',
   'e2e_ws_rt_1771439577160' => 'Valeur de test WebSocket en temps réel',
   'goodbye' => 'Au revoir',
