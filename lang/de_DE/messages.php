f5da02d40418df964493d65c314ddaf118ceb79a
LocaleFlow <noreply@localeflow.io>
2026-02-19 18:09:46 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771524053251' => 'Hallo vom grundlegenden Übersetzungstest',
-  'e2e_delete_tr_1771524531458' => 'Zu löschender Wert',
   'e2e_modify_tr_1771524346193' => 'Geänderter Wert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771524053251' => 'Bonjour, test de traduction de base',
-  'e2e_delete_tr_1771524531458' => 'Valeur à supprimer',
   'e2e_modify_tr_1771524346193' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name!',
