825f8625fd944e8c40de3e792cad875824eca993
LocaleFlow <noreply@localeflow.io>
2026-02-19 18:09:38 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1771524053251' => 'Halló frá grunnþýðingarprófun',
+  'e2e_delete_tr_1771524531458' => 'Gildi til að eyða',
   'e2e_modify_tr_1771524346193' => 'Breytt gildi fyrir próf á breytingu',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1771524053251' => 'Hallo uit een basisvertalingstest',
+  'e2e_delete_tr_1771524531458' => 'Te verwijderen waarde',
   'e2e_modify_tr_1771524346193' => 'Gewijzigde waarde voor wijzigingstest',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
