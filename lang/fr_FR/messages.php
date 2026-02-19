0e8216890dae904d80d0869e5e27955d76bb483d
LocaleFlow <noreply@localeflow.io>
2026-02-19 17:26:55 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771521650261' => 'Bonjour depuis le test de traduction basique',
-  'e2e_delete_tr_1771521979109' => 'Valeur à supprimer',
   'e2e_modify_tr_1771521693528' => 'Valeur modifiée pour le test de modifier',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771521650261' => 'Halló frá grunnþýðingarprófi',
-  'e2e_delete_tr_1771521979109' => 'Gildi til að eyða',
   'e2e_modify_tr_1771521693528' => 'Breytt gildi fyrir breytingarprófun',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
