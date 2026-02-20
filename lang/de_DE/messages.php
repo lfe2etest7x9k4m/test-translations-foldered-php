c3620d8d153ec58f2395b35b6a422c2f4835a6d8
LocaleFlow <noreply@localeflow.io>
2026-02-20 11:52:09 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1771588252483' => 'Hallo von grundlegendem Übersetzungstest',
-  'e2e_modify_tr_1771588284186' => 'Anfangswert für Änderungstest',
+  'e2e_modify_tr_1771588284186' => 'Geänderter Wert für Modifikationstest',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Willkommen bei unserer Anwendung',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1771588252483' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_modify_tr_1771588284186' => 'Upphaflegt gildi fyrir prófun á breytingu',
+  'e2e_modify_tr_1771588284186' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
