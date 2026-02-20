7ba7b11f1b0e606f42d0b9aade926bcf8b2db936
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 08:54:27 +0000

    E2E test: Delete e2e_delete_tr_1771577640345 from en_GB

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771577538441' => 'Hello from basic translation test',
-  'e2e_delete_tr_1771577640345' => 'Value to delete',
-  'e2e_modify_tr_1771577575407' => 'Modified value for modify test',
+  'e2e_basic_tr_1771577538441' => 'Hello from basic translation test',  'e2e_modify_tr_1771577575407' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
