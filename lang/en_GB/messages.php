f4819507dc939e29caaca7cf15a9495a5c9b7f5c
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 13:14:33 +0000

    Test EDGE-02: Modify same key in en_GB and en_US

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771592551426' => 'Hello from basic translation test',
   'e2e_context_tr_1771592691794' => 'Please use formal language for this text',
   'e2e_modify_tr_1771592583408' => 'Modified value for modify test',
-  'e2e_multi_lang_1771593239233' => 'en_GB original',
+  'e2e_multi_lang_1771593239233' => 'British English manual edit',
   'e2e_pr_tr_1771593125763' => 'PR test value',
   'e2e_quota_pause_1771593068045' => 'Quota pause test',
   'e2e_rt05_ws_1771593080605' => 'RT-05 WebSocket test',
--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771592551426' => 'Hello from basic translation test',
   'e2e_context_tr_1771592691794' => 'Please use formal language for this text',
   'e2e_modify_tr_1771592583408' => 'Modified value for modify test',
-  'e2e_multi_lang_1771593239233' => 'en_US original',
+  'e2e_multi_lang_1771593239233' => 'American English manual edit',
   'e2e_pr_tr_1771593125763' => 'PR test value',
   'e2e_rt21_ws_1771592892403' => 'RT-21 usage update test',
   'e2e_update_precedence_key' => 'UPDATED VALUE',
