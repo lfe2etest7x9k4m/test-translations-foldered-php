45cb17f0f55885d0d4284fb6dedf8e5138e55da1
LocaleFlow <noreply@localeflow.io>
2026-02-20 13:01:43 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -1,15 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771591078899' => 'Bonjour depuis le test de traduction simple',
-  'e2e_context_tr_1771591229162' => 'Please use formal language for this text',
-  'e2e_modify_tr_1771591115839' => 'Valeur modifiée pour le test de modification',
-  'e2e_pr_tr_1771591797133' => 'Valeur de test pour PR',
-  'e2e_rt21_ws_1771591469387' => 'Test de mise à jour de l\'utilisation de RT-21',
-  'e2e_update_precedence_key' => 'fr_FR original',
-  'e2e_ws_rt_1771591373063' => 'Valeur de test WebSocket en temps réel',
   'goodbye' => 'Au revoir',
-  'greeting' => 'Bonjour, :name !',
-  'quota_test_key' => 'Valeur de test',
+  'greeting' => 'Bonjour, :name!',
   'welcome' => 'Bienvenue sur notre application',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,15 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771591078899' => 'Hallo vanuit de basisvertalingstest',
-  'e2e_context_tr_1771591229162' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_modify_tr_1771591115839' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_pr_tr_1771591797133' => 'PR testwaarde',
-  'e2e_rt21_ws_1771591469387' => 'RT-21 test van gebruiksupdate',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'e2e_ws_rt_1771591373063' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
 ];
