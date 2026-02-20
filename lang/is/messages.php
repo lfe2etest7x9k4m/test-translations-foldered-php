67d23478226dff8bd216b63813c112b3a910fcfc
LocaleFlow <noreply@localeflow.io>
2026-02-20 10:20:20 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771581992961' => 'Halló frá grunnþýðingarprófun',
   'e2e_context_tr_1771582138207' => 'Vinsamlegast notið formlegt tungutak í þessum texta',
   'e2e_modify_tr_1771582030366' => 'Breytt gildi fyrir breytingarpróf',
-  'e2e_multi_lang_1771582756748' => 'is original',
+  'e2e_multi_lang_1771582756748' => 'Handvirk breyting á breskri ensku',
   'e2e_pr_tr_1771582649470' => 'Prófunargildi fyrir PR',
   'e2e_rt21_ws_1771582356973' => 'RT-21 próf á notkunaruppfærslu',
   'e2e_update_precedence_key' => 'is original',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771581992961' => 'Hallo vanuit de basisvertalingstest',
   'e2e_context_tr_1771582138207' => 'Gelieve formele taal te gebruiken voor deze tekst',
   'e2e_modify_tr_1771582030366' => 'Gewijzigde waarde voor test voor wijziging',
-  'e2e_multi_lang_1771582756748' => 'nl_NL original',
+  'e2e_multi_lang_1771582756748' => 'Handmatige bewerking Brits Engels',
   'e2e_pr_tr_1771582649470' => 'PR testwaarde',
   'e2e_rt21_ws_1771582356973' => 'RT-21 test van de gebruiksupdate',
   'e2e_update_precedence_key' => 'nl_NL original',
