898e3790f0828b15983e914be22cf728333bef90
LocaleFlow <noreply@localeflow.io>
2026-02-20 12:40:23 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771591078899' => 'Hallo von einem einfachen Übersetzungstest',
-  'e2e_delete_tr_1771591186082' => 'Zu löschender Wert',
   'e2e_modify_tr_1771591115839' => 'Modifikationstest',
   'goodbye' => 'Tschüss',
   'greeting' => 'Hallo, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771591078899' => 'Halló frá grunnprófun í þýðingu',
-  'e2e_delete_tr_1771591186082' => 'Gildi til að eyða',
   'e2e_modify_tr_1771591115839' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
