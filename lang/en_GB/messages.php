b82cb0bb9a128efe31f7f81de9892ba50876546d
LocaleFlow <noreply@localeflow.io>
2026-02-20 11:03:30 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -16,5 +16,4 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
-    'e2e_bp03_1771585401629' => 'GitHub protection test',
 ];
