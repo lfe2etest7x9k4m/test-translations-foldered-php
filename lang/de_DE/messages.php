39c347d72eaf604fbf75e9ece993e6f7fe319805
LocaleFlow <noreply@localeflow.io>
2026-02-20 11:50:10 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -3,5 +3,5 @@
 return [
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
-  'welcome' => 'Willkommen bei unserer Anwendung',
+  'welcome' => 'Willkommen zu unserer Anwendung',
 ];
