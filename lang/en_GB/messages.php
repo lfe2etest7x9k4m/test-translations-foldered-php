a28994dba5b42634c50b5dfe992d62232b9d8059
LocaleFlow <noreply@localeflow.io>
2026-02-20 11:56:31 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -4,10 +4,9 @@ return [
   'e2e_basic_tr_1771588252483' => 'Hello from basic translation test',
   'e2e_context_tr_1771588381643' => 'Please use formal language for this text',
   'e2e_modify_tr_1771588284186' => 'Modified value for modify test',
+  'e2e_rt24_ws_1771588551338' => 'RT-24 auto top-up test',
   'e2e_ws_rt_1771588483131' => 'WebSocket real-time test value',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
-    'e2e_rt24_ws_1771588551338' => 'RT-24 auto top-up test',
-    'e2e_rt21_ws_1771588572871' => 'RT-21 usage update test',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771588252483' => 'Halló frá einföldu þýðingarprófi',
   'e2e_context_tr_1771588381643' => 'Vinsamlegast notið formlega framsetningu fyrir þennan texta',
   'e2e_modify_tr_1771588284186' => 'Breytt gildi fyrir breytingarpróf',
+  'e2e_rt24_ws_1771588551338' => 'RT-24 próf á sjálfvirkri áfyllingu',
   'e2e_ws_rt_1771588483131' => 'Prófunargildi fyrir WebSocket í rauntíma',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
