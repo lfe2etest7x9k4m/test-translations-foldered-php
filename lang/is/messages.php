36207c17d38eb5711f25a946e71f2a373690042b
LocaleFlow <noreply@localeflow.io>
2026-02-20 10:50:57 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,6 +2,7 @@
 
 return [
   'e2e_basic_tr_1771584530478' => 'Halló frá einföldu þýðingarprófi',
+  'e2e_delete_tr_1771584632473' => 'Eyða gildi',
   'e2e_modify_tr_1771584567334' => 'Breytt gildi fyrir breytingarpróf',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
