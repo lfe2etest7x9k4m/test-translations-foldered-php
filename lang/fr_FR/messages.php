c7f8ddd37b4a1a0b23530bcca1d353f2c0f68f18
LocaleFlow <noreply@localeflow.io>
2026-02-20 08:47:27 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771576893859' => 'Bonjour depuis le test de traduction de base',
   'e2e_context_tr_1771577039010' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_modify_tr_1771576925448' => 'Valeur modifiée pour tester la modification',
+  'e2e_rt24_ws_1771577219775' => 'Test de recharge automatique RT-24',
   'e2e_ws_rt_1771577145587' => 'Valeur de test en temps réel WebSocket',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name!',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771576893859' => 'Halló frá einfaldri þýðingarprófi',
   'e2e_context_tr_1771577039010' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
   'e2e_modify_tr_1771576925448' => 'Breytt gildi fyrir prófun á breytingu',
+  'e2e_rt24_ws_1771577219775' => 'RT-24 próf á sjálfvirkri áfyllingu',
   'e2e_ws_rt_1771577145587' => 'WebSocket rauntíma prófunargildi',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
