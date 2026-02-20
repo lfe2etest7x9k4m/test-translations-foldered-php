467391c4551fbcc9f14101490b4001939b60f853
LocaleFlow <noreply@localeflow.io>
2026-02-20 11:09:34 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,17 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771584530478' => 'Hallo vanuit een basale vertaaltest',
-  'e2e_context_tr_1771584670113' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_err10_1771585361907' => 'Test voor afwijzing van push',
-  'e2e_modify_tr_1771584567334' => 'Gewijzigde waarde voor test voor wijziging',
-  'e2e_multi_lang_1771585269716' => 'Handmatige Brits-Engelse aanpassing',
-  'e2e_pr_tr_1771585145903' => 'PR test value',
-  'e2e_rt21_ws_1771584860937' => 'RT-21 gebruiksupdate test',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'e2e_ws_rt_1771584772497' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
   'welcome' => 'Welkom bij onze applicatie',
 ];
