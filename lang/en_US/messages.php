9c2ee204a5e5579f55c11a25cd6028553e54cfad
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 12:58:48 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,15 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771591078899' => 'Hello from basic translation test',
-  'e2e_context_tr_1771591229162' => 'Please use formal language for this text',
-  'e2e_modify_tr_1771591115839' => 'Modified value for modify test',
-  'e2e_pr_tr_1771591797133' => 'PR test value',
-  'e2e_rt21_ws_1771591469387' => 'RT-21 usage update test',
-  'e2e_ws_rt_1771591373063' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
-    'e2e_update_precedence_key' => 'UPDATED VALUE',
 ];
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,15 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771591078899' => 'Halló frá grunnprófun í þýðingu',
-  'e2e_context_tr_1771591229162' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
-  'e2e_modify_tr_1771591115839' => 'Breytt gildi fyrir breytingarpróf',
-  'e2e_pr_tr_1771591797133' => 'PR prófgildi',
-  'e2e_rt21_ws_1771591469387' => 'Próf á notkunaruppfærslu fyrir RT-21',
-  'e2e_ws_rt_1771591373063' => 'Prófunargildi fyrir WebSocket í rauntíma',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
-  'quota_test_key' => 'Prófunargildi',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
-    'e2e_update_precedence_key' => 'is original',
 ];
