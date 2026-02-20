03c1900a7a405b1eed46f6df8259f61357d8bd65
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 08:36:12 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771573947023' => 'Halló frá grunnprófun í þýðingu',
-  'e2e_context_tr_1771574071235' => 'Please use formal language for this text',
-  'e2e_modify_tr_1771573978762' => 'Breytt gildi fyrir breytingarprófun',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
