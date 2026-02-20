d9ce982e32afbe356ca25e6068891cf2cb5fff11
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 12:02:07 +0000

    Reset test repo to clean state

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -1,12 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771588252483' => 'Halló frá einföldu þýðingarprófi',
-  'e2e_context_tr_1771588381643' => 'Vinsamlegast notið formlega framsetningu fyrir þennan texta',
-  'e2e_modify_tr_1771588284186' => 'Breytt gildi fyrir breytingarpróf',
-  'e2e_rt21_ws_1771588572871' => 'Próf á uppfærslu á notkun RT-21',
-  'e2e_ws_rt_1771588483131' => 'Prófunargildi fyrir WebSocket í rauntíma',
-  'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
+  'goodbye' => 'Bless',
   'welcome' => 'Velkomin í forritið okkar',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,12 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771588252483' => 'Hallo vanuit de basisvertalingstest',
-  'e2e_context_tr_1771588381643' => 'Gebruik alstublieft formele taal voor deze tekst',
-  'e2e_modify_tr_1771588284186' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_rt21_ws_1771588572871' => 'RT-21 test van gebruiksupdate',
-  'e2e_ws_rt_1771588483131' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
