4a8bad07d58571ec91189e4d6e77cb713985b8d7
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 11:02:44 +0000

    E2E test: Add e2e_err10_1771585361907 for ERR-10 push rejection fallback

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -15,4 +15,5 @@ return [
   'greeting' => 'Hello, :name!',
   'quota_test_key' => 'Test value',
   'welcome' => 'Welcome to our application',
+    'e2e_err10_1771585361907' => 'Push rejection test',
 ];
