01990453d24709a6fcd86094fbe832498975e25a
LocaleFlow <noreply@localeflow.io>
2026-02-20 07:31:40 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1771572615450' => 'Bonjour depuis le test de traduction de base',
-  'e2e_modify_tr_1771572641829' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1771572641829' => 'Valeur modifiée pour le test de modifier',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name!',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1771572615450' => 'Halló frá prófun á grunnþýðingu',
-  'e2e_modify_tr_1771572641829' => 'Upphaflegt gildi fyrir prófun á breytingu',
+  'e2e_modify_tr_1771572641829' => 'Breytt gildi fyrir breytingarprófun',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
