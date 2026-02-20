d3ea17aa80510ce1656b174db707fb91b5e97e32
LocaleFlow <noreply@localeflow.io>
2026-02-20 13:12:57 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -6,6 +6,7 @@ return [
   'e2e_modify_tr_1771592583408' => 'Geänderter Wert für den Änderungs-Test',
   'e2e_pr_tr_1771593125763' => 'PR test value',
   'e2e_rt21_ws_1771592892403' => 'Test der RT-21 Nutzungsaktualisierung {{language}}',
+  'e2e_update_precedence_key' => 'de_DE original',
   'e2e_ws_rt_1771592804506' => 'WebSocket Echtzeit-Testwert',
   'goodbye' => 'Auf Wiedersehen',
   'greeting' => 'Hallo, :name!',
