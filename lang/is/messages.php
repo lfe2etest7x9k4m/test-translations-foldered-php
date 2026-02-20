38d9b002845835299c4797a38a108e5f59516c81
LocaleFlow <noreply@localeflow.io>
2026-02-20 12:45:48 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771591078899' => 'Halló frá grunnprófun í þýðingu',
   'e2e_context_tr_1771591229162' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
   'e2e_modify_tr_1771591115839' => 'Breytt gildi fyrir breytingarpróf',
-  'e2e_rt24_ws_1771591446228' => 'RT-24 próf fyrir sjálfvirkri áfyllingu',
+  'e2e_rt21_ws_1771591469387' => 'Próf á notkunaruppfærslu fyrir RT-21',
   'e2e_ws_rt_1771591373063' => 'Prófunargildi fyrir WebSocket í rauntíma',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,7 +4,7 @@ return [
   'e2e_basic_tr_1771591078899' => 'Hallo vanuit de basisvertalingstest',
   'e2e_context_tr_1771591229162' => 'Gelieve formele taal te gebruiken voor deze tekst',
   'e2e_modify_tr_1771591115839' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_rt24_ws_1771591446228' => 'RT-24 automatische opwaarderingstest',
+  'e2e_rt21_ws_1771591469387' => 'RT-21 test van gebruiksupdate',
   'e2e_ws_rt_1771591373063' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
