468739eba6921ab018a8412313931425b10b5c00
LocaleFlow <noreply@localeflow.io>
2026-02-20 13:04:49 +0000

    Automatic translations

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771592551426' => 'Hello from basic translation test',
-  'e2e_delete_tr_1771592648392' => 'Value to delete',
   'e2e_modify_tr_1771592583408' => 'Modified value for modify test',
   'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771592551426' => 'Bonjour depuis le test de traduction simple',
-  'e2e_delete_tr_1771592648392' => 'Valeur à supprimer',
   'e2e_modify_tr_1771592583408' => 'Valeur modifiée pour le test de modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
