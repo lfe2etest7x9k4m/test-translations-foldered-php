003c1c9657d86d0af1867c152b91cd23d587bfe3
LocaleFlow <noreply@localeflow.io>
2026-02-20 09:21:05 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771578999460' => 'Hallo van de basistest',
   'e2e_context_tr_1771579139257' => '',
   'e2e_modify_tr_1771579031305' => 'Gewijzigde waarde voor de wijzigingstest',
+  'e2e_ws_rt_1771579236081' => 'real-time testwaarde voor WebSocket',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
   'welcome' => 'Welkom bij onze applicatie',
