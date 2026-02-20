8dbeae708fe4bce4ea43f52e9a8c165161419d15
LocaleFlow <noreply@localeflow.io>
2026-02-20 08:54:31 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,6 @@
 
 return [
   'e2e_basic_tr_1771577538441' => 'Bonjour depuis le test de traduction basique',
-  'e2e_delete_tr_1771577640345' => 'Valeur à supprimer',
   'e2e_modify_tr_1771577575407' => 'Valeur modifiée pour tester la modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name!',
