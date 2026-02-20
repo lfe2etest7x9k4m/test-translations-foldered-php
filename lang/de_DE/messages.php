a9b3111e617c35051b689a738efc760bd6687a86
LocaleFlow <noreply@localeflow.io>
2026-02-20 13:51:58 +0000

    Automatic translations

--- lang/de_DE/messages.php
+++ lang/de_DE/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1771594630036' => 'Hallo vom grundlegenden Übersetzungstest',
   'e2e_context_tr_1771594775878' => 'Bitte verwenden Sie für diesen Text eine formelle Sprache',
+  'e2e_err10_1771595482020' => 'Test der Push-Zurückweisung',
   'e2e_modify_tr_1771594661950' => 'Geänderter Wert für den Modifikationstest',
   'e2e_multi_lang_1771595374468' => 'Manuelle Bearbeitung in britischem Englisch',
   'e2e_pr_tr_1771595251158' => 'PR test value',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -3,6 +3,7 @@
 return [
   'e2e_basic_tr_1771594630036' => 'Halló frá einfaldri þýðingarprófun',
   'e2e_context_tr_1771594775878' => 'Vinsamlegast notið formlegt tungumál fyrir þennan texta',
+  'e2e_err10_1771595482020' => 'Prófun á höfnun við ýtingu',
   'e2e_modify_tr_1771594661950' => 'Breytt gildi fyrir breytingarprófun',
   'e2e_multi_lang_1771595374468' => 'Handvirk breyting á breskri ensku',
   'e2e_pr_tr_1771595251158' => 'Prófunargildi fyrir PR',
