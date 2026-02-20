96d0aa306148e83cc2314ab9b229210c860bdce9
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 11:48:52 +0000

    Reset test repo to clean state

--- lang/en_US/messages.php
+++ lang/en_US/messages.php
@@ -1,18 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771585816870' => 'Hello from basic translation test',
-  'e2e_bp03_1771586694655' => 'GitHub protection test',
-  'e2e_context_tr_1771585967381' => 'Please use formal language for this text',
-  'e2e_err10_1771586649922' => 'Push rejection test',
-  'e2e_modify_tr_1771585854090' => 'Modified value for modify test',
-  'e2e_multi_lang_1771586579160' => 'American English manual edit',
-  'e2e_pr_tr_1771586450884' => 'PR test value',
-  'e2e_rt21_ws_1771586165320' => 'RT-21 usage update test',
-  'e2e_update_precedence_key' => 'UPDATED VALUE',
-  'e2e_ws_rt_1771586065154' => 'WebSocket real-time test value',
-  'goodbye' => 'Goodbye',
   'greeting' => 'Hello, :name!',
-  'quota_test_key' => 'Test value',
+  'goodbye' => 'Goodbye',
   'welcome' => 'Welcome to our application',
 ];
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -1,18 +1,7 @@
 <?php
 
 return [
-  'e2e_basic_tr_1771585816870' => 'Hallo vanuit de basisvertalingstest',
-  'e2e_bp03_1771586694655' => 'GitHub beschermingstest',
-  'e2e_context_tr_1771585967381' => 'Gelieve formele taal te gebruiken voor deze tekst',
-  'e2e_err10_1771586649922' => 'Test van push-afwijzing',
-  'e2e_modify_tr_1771585854090' => 'Gewijzigde waarde voor wijzigingstest',
-  'e2e_multi_lang_1771586579160' => 'Handmatige bewerking in Brits-Engels',
-  'e2e_pr_tr_1771586450884' => 'PR testwaarde',
-  'e2e_rt21_ws_1771586165320' => 'RT-21 test van gebruiksupdate',
-  'e2e_update_precedence_key' => 'nl_NL original',
-  'e2e_ws_rt_1771586065154' => 'WebSocket real-time testwaarde',
-  'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
-  'quota_test_key' => 'Testwaarde',
+  'goodbye' => 'Tot ziens',
   'welcome' => 'Welkom bij onze applicatie',
 ];
