c1887d70c3978dc6be01f9d1d88dbbdd93756873
lfe2etest7x9k4m <test@localeflow.ai>
2026-02-20 13:42:37 +0000

    Test RT-21: e2e_rt21_ws_1771594957397

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -9,4 +9,5 @@ return [
   'greeting' => 'Hello, :name!',
   'welcome' => 'Welcome to our application',
     'e2e_rt24_ws_1771594937853' => 'RT-24 auto top-up test',
+    'e2e_rt21_ws_1771594957397' => 'RT-21 usage update test',
 ];
