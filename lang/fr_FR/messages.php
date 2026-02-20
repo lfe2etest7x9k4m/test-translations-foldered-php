82997d3a942a302bebfbdd4e99ed90977eebcecd
LocaleFlow <noreply@localeflow.io>
2026-02-20 10:50:26 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'e2e_basic_tr_1771584530478' => 'Bonjour, ceci est un test de traduction de base',
-  'e2e_modify_tr_1771584567334' => 'Valeur initiale pour le test de modification',
+  'e2e_modify_tr_1771584567334' => 'Valeur modifiée pour tester la modification',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
