2910d8386ba1711702035f93edd1a2f4ea17aa14
LocaleFlow <noreply@localeflow.io>
2026-02-20 13:07:05 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++ lang/fr_FR/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771592551426' => 'Bonjour depuis le test de traduction simple',
   'e2e_context_tr_1771592691794' => 'Veuillez utiliser un langage formel pour ce texte',
   'e2e_modify_tr_1771592583408' => 'Valeur modifiée pour le test de modification',
+  'e2e_ws_rt_1771592804506' => 'Valeur de test WebSocket en temps réel',
   'goodbye' => 'Au revoir',
   'greeting' => 'Bonjour, :name !',
   'welcome' => 'Bienvenue sur notre application',
--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,7 @@ return [
   'e2e_basic_tr_1771592551426' => 'Halló frá grunnþýðingarprófi',
   'e2e_context_tr_1771592691794' => 'Please use formal language for this text',
   'e2e_modify_tr_1771592583408' => 'Breytt gildi fyrir próf á breytingu',
+  'e2e_ws_rt_1771592804506' => 'Gildi fyrir rauntíma prófun á WebSocket',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
   'welcome' => 'Velkomin í forritið okkar',
