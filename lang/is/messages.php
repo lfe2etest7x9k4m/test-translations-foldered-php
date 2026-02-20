5d69dadac6fc0ab160aa5be96fd678b8fb485b6c
LocaleFlow <noreply@localeflow.io>
2026-02-20 12:16:14 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -6,7 +6,7 @@ return [
   'e2e_modify_tr_1771589059033' => 'Breytt gildi fyrir breyta próf',
   'e2e_pr_tr_1771589684794' => 'PR prófun',
   'e2e_rt21_ws_1771589391570' => 'Próf á uppfærslu á notkun RT-21',
-  'e2e_update_precedence_key' => 'is original',
+  'e2e_update_precedence_key' => 'UPPFÆRT GILDI',
   'e2e_ws_rt_1771589296018' => 'WebSocket rauntíma prófunarverðmæti',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name!',
--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -6,7 +6,7 @@ return [
   'e2e_modify_tr_1771589059033' => 'Gewijzigde waarde voor testaanpassing',
   'e2e_pr_tr_1771589684794' => 'PR testwaarde',
   'e2e_rt21_ws_1771589391570' => 'RT-21 test voor gebruiksupdate',
-  'e2e_update_precedence_key' => 'nl_NL original',
+  'e2e_update_precedence_key' => 'UPDATED VALUE',
   'e2e_ws_rt_1771589296018' => 'WebSocket real-time testwaarde',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name!',
