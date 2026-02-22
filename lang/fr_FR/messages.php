83419ff069f9a0f46eecbf7276c7cf7815f4e57f
LocaleFlow <noreply@localeflow.io>
2026-02-22 05:23:22 +0000

    Automatic translations

--- lang/fr_FR/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-  'error_message' => 'Un problème est survenu',
-  'goodbye' => 'Au revoir',
-  'greeting' => 'Bonjour, :name\\ !',
-  'success' => 'Opération terminée avec succès',
-  'welcome' => 'Bienvenue sur notre application',
-];
