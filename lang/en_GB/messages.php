c81d281752f6ba280a3fe55c9bca670db979f8b8
LocaleFlow <noreply@localeflow.io>
2026-02-24 09:53:35 +0000

    Automatic translations

--- lang/en_GB/messages.php
+++ lang/en_GB/messages.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-    'greeting' => 'Hello, :name\!',
-    'goodbye' => 'Goodbye',
-    'welcome' => 'Welcome to our application',
-    'error_message' => 'Something went wrong',
-    'success' => 'Operation completed successfully',
+  'goodbye' => 'Goodbye',
+  'greeting' => 'Hello, :name!',
+  'welcome' => 'Welcome to our application',
 ];
---
+++ lang/is_IS/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'goodbye' => 'Bless',
+  'greeting' => 'Hæ, :name\\!',
+  'welcome' => 'Velkomin í forritið okkar',
+];
