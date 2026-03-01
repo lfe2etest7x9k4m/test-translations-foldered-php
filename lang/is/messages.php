4cab3f6fef752f91f6ad617da6b7004332904efc
LocaleFlow <noreply@localeflow.io>
2026-03-01 01:21:38 +0000

    Automatic translations

--- lang/is/messages.php
+++ lang/is/messages.php
@@ -4,6 +4,6 @@ return [
   'error_message' => 'Eitthvað fór úrskeiðis',
   'goodbye' => 'Bless',
   'greeting' => 'Halló, :name\\!',
-  'success' => 'Aðgerð tókst með góðum árangri',
+  'success' => 'Aðgerðinni hefur verið lokið með góðum árangri',
   'welcome' => 'Velkomin í forritið okkar',
 ];
