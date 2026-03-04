f2363794a188085b501ae4d3435a552c87d21215
LocaleFlow <noreply@localeflow.io>
2026-03-04 06:42:42 +0000

    Automatic translations

--- lang/nl_NL/messages.php
+++ lang/nl_NL/messages.php
@@ -4,6 +4,6 @@ return [
   'error_message' => 'Er is iets misgegaan',
   'goodbye' => 'Tot ziens',
   'greeting' => 'Hallo, :name\\!',
-  'success' => 'Operatie succesvol voltooid',
+  'success' => 'Bewerking succesvol voltooid',
   'welcome' => 'Welkom bij onze applicatie',
 ];
